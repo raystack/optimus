5707c5e8f6ac2d48820f11391dc6512d5f4a0b8e
Arinda Arif <arindaarif05@gmail.com>
2021-05-31 16:40:02 +0700

    fix: change base_dag of airflow 2 to follow plugin changes

--- resources/pack/templates/scheduler/airflow_2/base_dag.py
+++ resources/pack/templates/scheduler/airflow_2/base_dag.py
@@ -13,21 +13,10 @@ from kubernetes.client import models as k8s
 from __lib import alert_failed_to_slack, SuperKubernetesPodOperator, SuperExternalTaskSensor, \
     SlackWebhookOperator, CrossTenantDependencySensor
 
-
-SECRET_NAME = Variable.get("secret_name", "optimus-google-credentials")
-SECRET_KEY = Variable.get("secret_key", "auth.json")
-SECRET_VOLUME_PATH = '/opt/optimus/secrets/'
 SENSOR_DEFAULT_POKE_INTERVAL_IN_SECS = int(Variable.get("sensor_poke_interval_in_secs", default_var=15 * 60))
 SENSOR_DEFAULT_TIMEOUT_IN_SECS = int(Variable.get("sensor_timeout_in_secs", default_var=15 * 60 * 60))
 DAG_RETRIES = int(Variable.get("dag_retries", default_var=3))
 DAG_RETRY_DELAY = int(Variable.get("dag_retry_delay_in_secs", default_var=5 * 60))
-
-gcloud_credentials_path = '{}{}'.format(SECRET_VOLUME_PATH, SECRET_KEY)
-gcloud_secret = Secret(
-    'volume',
-    SECRET_VOLUME_PATH,
-    SECRET_NAME,
-    SECRET_KEY)
 
 default_args = {
     "owner": {{.Job.Owner | quote}},
@@ -48,28 +37,37 @@ dag = DAG(
     catchup ={{ if .Job.Behavior.CatchUp }} True{{ else }} False{{ end }}
 )
 
-transformation_{{.Job.Task.Unit.Name | replace "-" "__dash__" | replace "." "__dot__"}} = SuperKubernetesPodOperator(
+{{$baseTaskSchema := .Job.Task.Unit.GetTaskSchema $.Context $.TaskSchemaRequest -}}
+{{ if ne $baseTaskSchema.SecretPath "" -}}
+transformation_secret = Secret(
+    "volume",
+    {{ dir $baseTaskSchema.SecretPath | quote }},
+    "optimus-task-{{ $baseTaskSchema.Name }}",
+    {{ base $baseTaskSchema.SecretPath | quote }}
+)
+{{- end }}
+transformation_{{$baseTaskSchema.Name | replace "-" "__dash__" | replace "." "__dot__"}} = SuperKubernetesPodOperator(
     image_pull_policy="Always",
     namespace = conf.get('kubernetes', 'namespace', fallback="default"),
-    image = "{}".format("{{.Job.Task.Unit.Image}}"),
+    image = {{ $baseTaskSchema.Image | quote}},
     cmds=[],
-    name="{{.Job.Task.Unit.Name | replace "_" "-" }}",
-    task_id={{.Job.Task.Unit.Name | quote}},
+    name="{{ $baseTaskSchema.Name | replace "_" "-" }}",
+    task_id={{$baseTaskSchema.Name | quote}},
     get_logs=True,
     dag=dag,
     in_cluster=True,
     is_delete_operator_pod=True,
     do_xcom_push=False,
-    secrets=[gcloud_secret],
+    secrets=[{{ if ne $baseTaskSchema.SecretPath "" -}} transformation_secret {{- end }}],
     env_vars = [
-      k8s.V1EnvVar(name="GOOGLE_APPLICATION_CREDENTIALS",value=gcloud_credentials_path),
+      k8s.V1EnvVar(name="GOOGLE_APPLICATION_CREDENTIALS",value='{{ $baseTaskSchema.SecretPath }}'),
       k8s.V1EnvVar(name="JOB_NAME",value='{{.Job.Name}}'),
       k8s.V1EnvVar(name="OPTIMUS_HOSTNAME",value='{{.Hostname}}'),
       k8s.V1EnvVar(name="JOB_LABELS",value='{{.Job.GetLabelsAsString}}'),
       k8s.V1EnvVar(name="JOB_DIR",value='/data'), 
       k8s.V1EnvVar(name="PROJECT",value='{{.Project.Name}}'),
-      k8s.V1EnvVar(name="TASK_TYPE",value='{{$.InstanceTypeTransformation}}'), 
-      k8s.V1EnvVar(name="TASK_NAME",value='{{.Job.Task.Unit.Name}}'),
+      k8s.V1EnvVar(name="TASK_TYPE",value='{{$.InstanceTypeTask}}'), 
+      k8s.V1EnvVar(name="TASK_NAME",value='{{$baseTaskSchema.Name}}'),
       k8s.V1EnvVar(name="SCHEDULED_AT",value='{{ "{{ next_execution_date }}" }}')
   	],
     reattach_on_restart=True,
@@ -77,28 +75,48 @@ transformation_{{.Job.Task.Unit.Name | replace "-" "__dash__" | replace "." "__d
 
 # hooks loop start
 {{ range $_, $t := .Job.Hooks }}
-hook_{{$t.Unit.Name}} = SuperKubernetesPodOperator(
+{{ $hookSchema := $t.Unit.GetHookSchema $.Context $.HookSchemaRequest -}}
+
+{{ if ne $hookSchema.SecretPath "" -}}
+hook_{{$hookSchema.Name | replace "-" "_"}}_secret = Secret(
+    "volume",
+    {{ dir $hookSchema.SecretPath | quote }},
+    "optimus-hook-{{ $hookSchema.Name }}",
+    {{ base $hookSchema.SecretPath | quote }}
+)
+{{- end -}}
+
+hook_{{$hookSchema.Name}} = SuperKubernetesPodOperator(
     image_pull_policy="Always",
     namespace = conf.get('kubernetes', 'namespace', fallback="default"),
-    image = "{{$t.Unit.Image}}",
+    image = "{{ $hookSchema.Image }}",
     cmds=[],
-    name="hook_{{$t.Unit.Name}}",
-    task_id="hook_{{$t.Unit.Name}}",
+    name="hook_{{ $hookSchema.Name | replace "_" "-"}}",
+    task_id="hook_{{ $hookSchema.Name }}",
     get_logs=True,
     dag=dag,
     in_cluster=True,
     is_delete_operator_pod=True,
     do_xcom_push=False,
-    secrets=[gcloud_secret],
-    env_vars = [
-      k8s.V1EnvVar(name="GOOGLE_APPLICATION_CREDENTIALS",value=gcloud_credentials_path),
+    secrets=[{{ if ne $hookSchema.SecretPath "" -}} hook_{{$hookSchema.Name | replace "-" "_"}}_secret {{- end }}],
+    env_vars={
+        "GOOGLE_APPLICATION_CREDENTIALS": '{{ $hookSchema.SecretPath }}',
+        "JOB_NAME":'{{$.Job.Name}}', "OPTIMUS_HOSTNAME":'{{$.Hostname}}',
+        "JOB_LABELS":'{{$.Job.GetLabelsAsString}}',
+        "JOB_DIR":'/data', "PROJECT":'{{$.Project.Name}}',
+        "TASK_TYPE":'{{$.InstanceTypeHook}}', "TASK_NAME":'{{$hookSchema.Name}}',
+        "SCHEDULED_AT":'{{ "{{ next_execution_date }}" }}',
+        # rest of the env vars are pulled from the container by making a GRPC call to optimus
+   },
+   env_vars = [
+      k8s.V1EnvVar(name="GOOGLE_APPLICATION_CREDENTIALS",value='{{ $hookSchema.SecretPath }}'),
       k8s.V1EnvVar(name="JOB_NAME",value='{{$.Job.Name}}'),
       k8s.V1EnvVar(name="OPTIMUS_HOSTNAME",value='{{$.Hostname}}'),
       k8s.V1EnvVar(name="JOB_LABELS",value='{{$.Job.GetLabelsAsString}}'),
       k8s.V1EnvVar(name="JOB_DIR",value='/data'), 
       k8s.V1EnvVar(name="PROJECT",value='{{$.Project.Name}}')
       k8s.V1EnvVar(name="TASK_TYPE",value='{{$.InstanceTypeHook}}'),
-      k8s.V1EnvVar(name="TASK_NAME",value='{{$t.Unit.Name}}'), 
+      k8s.V1EnvVar(name="TASK_NAME",value='{{$hookSchema.Name}}'), 
       k8s.V1EnvVar(name="SCHEDULED_AT",value='{{ "{{ next_execution_date }}" }}')
       # rest of the env vars are pulled from the container by making a GRPC call to optimus
   	],
@@ -111,6 +129,8 @@ hook_{{$t.Unit.Name}} = SuperKubernetesPodOperator(
 # create upstream sensors
 {{ $baseWindow := $.Job.Task.Window }}
 {{- range $_, $dependency := $.Job.Dependencies}}
+{{- $dependencySchema := $dependency.Job.Task.Unit.GetTaskSchema $.Context $.TaskSchemaRequest }}
+
 {{- if eq $dependency.Type $.JobSpecDependencyTypeIntra }}
 wait_{{$dependency.Job.Name | replace "-" "__dash__" | replace "." "__dot__"}} = SuperExternalTaskSensor(
     external_dag_id = "{{$dependency.Job.Name}}",
@@ -118,7 +138,7 @@ wait_{{$dependency.Job.Name | replace "-" "__dash__" | replace "." "__dot__"}} =
     window_offset = {{$baseWindow.Offset.String | quote}},
     window_truncate_to = {{$baseWindow.TruncateTo | quote}},
     optimus_hostname = "{{$.Hostname}}",
-    task_id = "wait_{{$dependency.Job.Name | trunc 200}}-{{$dependency.Job.Task.Unit.Name}}",
+    task_id = "wait_{{$dependency.Job.Name | trunc 200}}-{{$dependencySchema.Name}}",
     poke_interval = SENSOR_DEFAULT_POKE_INTERVAL_IN_SECS,
     timeout = SENSOR_DEFAULT_TIMEOUT_IN_SECS,
     dag=dag
@@ -132,7 +152,7 @@ wait_{{$dependency.Job.Name | replace "-" "__dash__" | replace "." "__dot__"}} =
     optimus_job="{{$dependency.Job.Name}}",
     poke_interval=SENSOR_DEFAULT_POKE_INTERVAL_IN_SECS,
     timeout=SENSOR_DEFAULT_TIMEOUT_IN_SECS,
-    task_id="wait_{{$dependency.Job.Name | trunc 200}}-{{$dependency.Job.Task.Unit.Name}}",
+    task_id="wait_{{$dependency.Job.Name | trunc 200}}-{{$dependencySchema.Name}}",
     dag=dag
 )
 {{- end -}}
@@ -143,22 +163,25 @@ wait_{{$dependency.Job.Name | replace "-" "__dash__" | replace "." "__dot__"}} =
 
 # upstream sensors -> base transformation task
 {{- range $i, $t := $.Job.Dependencies }}
-wait_{{ $t.Job.Name | replace "-" "__dash__" | replace "." "__dot__" }} >> transformation_{{$.Job.Task.Unit.Name | replace "-" "__dash__" | replace "." "__dot__"}}
+wait_{{ $t.Job.Name | replace "-" "__dash__" | replace "." "__dot__" }} >> transformation_{{$baseTaskSchema.Name | replace "-" "__dash__" | replace "." "__dot__"}}
 {{- end}}
 
 # set inter-dependencies between task and hooks
 {{- range $_, $task := .Job.Hooks }}
-{{- if eq $task.Unit.Type $.HookTypePre }}
-hook_{{$task.Unit.Name}} >> transformation_{{$.Job.Task.Unit.Name | replace "-" "__dash__" | replace "." "__dot__"}}
+{{- $hookSchema := $task.Unit.GetHookSchema $.Context $.HookSchemaRequest }}
+{{- if eq $hookSchema.Type $.HookTypePre }}
+hook_{{$hookSchema.Name}} >> transformation_{{$baseTaskSchema.Name | replace "-" "__dash__" | replace "." "__dot__"}}
 {{- end -}}
-{{- if eq $task.Unit.Type $.HookTypePost }}
-transformation_{{$.Job.Task.Unit.Name | replace "-" "__dash__" | replace "." "__dot__"}} >> hook_{{$task.Unit.Name}}
+{{- if eq $hookSchema.Type $.HookTypePost }}
+transformation_{{$baseTaskSchema.Name | replace "-" "__dash__" | replace "." "__dot__"}} >> hook_{{$hookSchema.Name}}
 {{- end -}}
 {{- end }}
 
 # set inter-dependencies between hooks and hooks
 {{- range $_, $t := .Job.Hooks }}
+{{- $hookSchema := $t.Unit.GetHookSchema $.Context $.HookSchemaRequest }}
 {{- range $_, $depend := $t.DependsOn }}
-hook_{{$depend.Unit.Name}} >> hook_{{$t.Unit.Name}}
+{{- $dependHookSchema := $depend.Unit.GetHookSchema $.Context $.HookSchemaRequest }}
+hook_{{$dependHookSchema.Name}} >> hook_{{$hookSchema.Name}}
 {{- end }}
 {{- end }}
