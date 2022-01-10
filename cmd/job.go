6e47ff74ec37660cab513716ce88bade2d465edd
Kush <thekushsharma@gmail.com>
2022-01-10 17:48:42 +0530

    refactor(cli): make project/namespace configs required in deploy cmd
    Signed-off-by: Kush <thekushsharma@gmail.com>

--- cmd/job.go
+++ cmd/job.go
@@ -4,13 +4,13 @@ import (
 	"github.com/odpf/optimus/config"
 
 	"github.com/odpf/optimus/models"
-	"github.com/odpf/optimus/store/local"
 	"github.com/odpf/salt/log"
 	"github.com/spf13/afero"
 	cli "github.com/spf13/cobra"
 )
 
-func jobCommand(l log.Logger, jobSpecFs afero.Fs, pluginRepo models.PluginRepository, conf config.Provider) *cli.Command {
+func jobCommand(l log.Logger, jobSpecFs afero.Fs, jobSpecRepo JobSpecRepository,
+	pluginRepo models.PluginRepository, conf config.Provider) *cli.Command {
 	cmd := &cli.Command{
 		Use:   "job",
 		Short: "Interact with schedulable Job",
@@ -18,17 +18,13 @@ func jobCommand(l log.Logger, jobSpecFs afero.Fs, pluginRepo models.PluginReposi
 			"group:core": "true",
 		},
 	}
-	jobSpecRepo := local.NewJobSpecRepository(
-		jobSpecFs,
-		local.NewJobSpecAdapter(pluginRepo),
-	)
-
-	cmd.AddCommand(jobCreateCommand(l, jobSpecFs, jobSpecRepo, pluginRepo))
-	cmd.AddCommand(jobAddHookCommand(l, jobSpecRepo, pluginRepo))
-	cmd.AddCommand(jobRenderTemplateCommand(l, jobSpecRepo))
-	cmd.AddCommand(jobValidateCommand(l, conf.GetHost(), pluginRepo, jobSpecRepo, conf))
-	cmd.AddCommand(jobRunCommand(l, jobSpecRepo, pluginRepo, conf))
+	if jobSpecRepo != nil {
+		cmd.AddCommand(jobCreateCommand(l, jobSpecFs, jobSpecRepo, pluginRepo))
+		cmd.AddCommand(jobAddHookCommand(l, jobSpecRepo, pluginRepo))
+		cmd.AddCommand(jobRenderTemplateCommand(l, jobSpecRepo))
+		cmd.AddCommand(jobValidateCommand(l, pluginRepo, jobSpecRepo, conf))
+		cmd.AddCommand(jobRunCommand(l, jobSpecRepo, pluginRepo, conf))
+	}
 	cmd.AddCommand(jobStatusCommand(l, conf))
-
 	return cmd
 }
