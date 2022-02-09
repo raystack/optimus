409a243865d886446c50329ab95fc4ea491952bf
Sandeep Bhardwaj <sandeep.bhardwaj@gojek.com>
2022-02-09 23:44:16 +0530

    feat: add methods to service for project and namespace

--- service/namespace_service_test.go
+++ service/namespace_service_test.go
@@ -20,8 +20,11 @@ func TestNamespaceService(t *testing.T) {
 		Name: "optimus-project",
 	}
 	namespace := models.NamespaceSpec{
-		ID:          uuid.New(),
-		Name:        "sample-namespace",
+		ID:   uuid.New(),
+		Name: "sample-namespace",
+		Config: map[string]string{
+			"bucket": "gs://some_folder",
+		},
 		ProjectSpec: project,
 	}
 	projService := new(mock.ProjectService)
@@ -47,7 +50,7 @@ func TestNamespaceService(t *testing.T) {
 			assert.NotNil(t, err)
 			assert.Equal(t, "project not found", err.Error())
 		})
-		t.Run("return error when projectService returns error", func(t *testing.T) {
+		t.Run("return error when repo returns error", func(t *testing.T) {
 			defer projService.AssertExpectations(t)
 
 			namespaceRepository := new(mock.NamespaceRepository)
@@ -85,6 +88,116 @@ func TestNamespaceService(t *testing.T) {
 
 			assert.Equal(t, "sample-namespace", ns.Name)
 			assert.Equal(t, namespace.ID, ns.ID)
+		})
+	})
+	t.Run("Save", func(t *testing.T) {
+		t.Run("return error when namespace name is invalid", func(t *testing.T) {
+			ns := models.NamespaceSpec{
+				ID:   uuid.New(),
+				Name: "",
+			}
+			svc := service.NewNamespaceService(nil, nil)
+
+			err := svc.Save(ctx, project.Name, ns)
+			assert.NotNil(t, err)
+			assert.Equal(t, "namespace name cannot be empty: invalid argument for entity namespace", err.Error())
+		})
+		t.Run("return error when namespace config is empty", func(t *testing.T) {
+			ns := models.NamespaceSpec{
+				ID:   uuid.New(),
+				Name: "name",
+			}
+			svc := service.NewNamespaceService(nil, nil)
+
+			err := svc.Save(ctx, project.Name, ns)
+			assert.NotNil(t, err)
+			assert.Equal(t, "namespace config cannot be empty: invalid argument for entity namespace", err.Error())
+		})
+		t.Run("returns error when fetching project fails", func(t *testing.T) {
+			defer projService.AssertExpectations(t)
+
+			namespaceRepository := new(mock.NamespaceRepository)
+			namespaceRepository.On("Save", ctx, namespace).
+				Return(store.ErrResourceNotFound)
+			defer namespaceRepository.AssertExpectations(t)
+
+			nsRepoFactory := new(mock.NamespaceRepoFactory)
+			nsRepoFactory.On("New", project).Return(namespaceRepository)
+			defer nsRepoFactory.AssertExpectations(t)
+
+			svc := service.NewNamespaceService(projService, nsRepoFactory)
+
+			err := svc.Save(ctx, project.Name, namespace)
+			assert.NotNil(t, err)
+			assert.Equal(t, "resource not found: not found for entity namespace", err.Error())
+		})
+		t.Run("calls repo to store project successfully", func(t *testing.T) {
+			defer projService.AssertExpectations(t)
+
+			namespaceRepository := new(mock.NamespaceRepository)
+			namespaceRepository.On("Save", ctx, namespace).Return(nil)
+			defer namespaceRepository.AssertExpectations(t)
+
+			nsRepoFactory := new(mock.NamespaceRepoFactory)
+			nsRepoFactory.On("New", project).Return(namespaceRepository)
+			defer nsRepoFactory.AssertExpectations(t)
+
+			svc := service.NewNamespaceService(projService, nsRepoFactory)
+
+			err := svc.Save(ctx, project.Name, namespace)
+			assert.Nil(t, err)
+		})
+	})
+	t.Run("GetAll", func(t *testing.T) {
+		t.Run("return error when getting project fails", func(t *testing.T) {
+			projService := new(mock.ProjectService)
+			projService.On("Get", ctx, "invalid").
+				Return(models.ProjectSpec{}, errors.New("project not found"))
+			defer projService.AssertExpectations(t)
+
+			svc := service.NewNamespaceService(projService, nil)
+
+			_, err := svc.GetAll(ctx, "invalid")
+			assert.NotNil(t, err)
+			assert.Equal(t, "project not found", err.Error())
+		})
+		t.Run("return error when repo has error", func(t *testing.T) {
+			defer projService.AssertExpectations(t)
+
+			namespaceRepo := new(mock.NamespaceRepository)
+			namespaceRepo.On("GetAll", ctx).
+				Return([]models.NamespaceSpec{}, errors.New("random error"))
+			defer namespaceRepo.AssertExpectations(t)
+
+			nsRepoFactory := new(mock.NamespaceRepoFactory)
+			nsRepoFactory.On("New", project).Return(namespaceRepo)
+			defer nsRepoFactory.AssertExpectations(t)
+
+			svc := service.NewNamespaceService(projService, nsRepoFactory)
+
+			_, err := svc.GetAll(ctx, project.Name)
+			assert.NotNil(t, err)
+			assert.Equal(t, ": internal error for entity namespace", err.Error())
+		})
+		t.Run("return namespaces successfully", func(t *testing.T) {
+			defer projService.AssertExpectations(t)
+
+			namespaceRepo := new(mock.NamespaceRepository)
+			namespaceRepo.On("GetAll", ctx).
+				Return([]models.NamespaceSpec{namespace}, nil)
+			defer namespaceRepo.AssertExpectations(t)
+
+			nsRepoFactory := new(mock.NamespaceRepoFactory)
+			nsRepoFactory.On("New", project).Return(namespaceRepo)
+			defer nsRepoFactory.AssertExpectations(t)
+
+			svc := service.NewNamespaceService(projService, nsRepoFactory)
+
+			namespaces, err := svc.GetAll(ctx, project.Name)
+			assert.Nil(t, err)
+			assert.Len(t, namespaces, 1)
+			assert.Equal(t, "sample-namespace", namespaces[0].Name)
+			assert.Equal(t, namespace.ID, namespaces[0].ID)
 		})
 	})
 }
--- service/project_service.go
+++ service/project_service.go
@@ -7,12 +7,16 @@ import (
 	"github.com/odpf/optimus/store"
 )
 
+const ProjectEntity = "project"
+
 type ProjectRepoFactory interface {
 	New() store.ProjectRepository
 }
 
 type ProjectService interface {
 	Get(context.Context, string) (models.ProjectSpec, error)
+	Save(context.Context, models.ProjectSpec) error
+	GetAll(context.Context) ([]models.ProjectSpec, error)
 }
 
 type projectService struct {
@@ -28,13 +32,39 @@ func NewProjectService(factory ProjectRepoFactory) *projectService {
 func (s projectService) Get(ctx context.Context, projectName string) (models.ProjectSpec, error) {
 	if projectName == "" {
 		return models.ProjectSpec{},
-			NewError("project", ErrInvalidArgument, "project name cannot be empty")
+			NewError(ProjectEntity, ErrInvalidArgument, "project name cannot be empty")
 	}
 
 	projectRepo := s.projectRepoFac.New()
 	projSpec, err := projectRepo.GetByName(ctx, projectName)
 	if err != nil {
-		return models.ProjectSpec{}, FromError(err, "project", "")
+		return models.ProjectSpec{}, FromError(err, ProjectEntity, "")
 	}
 	return projSpec, nil
+}
+
+func (s projectService) Save(ctx context.Context, project models.ProjectSpec) error {
+	if project.Name == "" {
+		return NewError(ProjectEntity, ErrInvalidArgument, "project name cannot be empty")
+	}
+
+	if len(project.Config) == 0 {
+		return NewError(ProjectEntity, ErrInvalidArgument, "project config cannot be empty")
+	}
+
+	projectRepo := s.projectRepoFac.New()
+	err := projectRepo.Save(ctx, project)
+	if err != nil {
+		return FromError(err, ProjectEntity, "")
+	}
+	return nil
+}
+
+func (s projectService) GetAll(ctx context.Context) ([]models.ProjectSpec, error) {
+	projectRepo := s.projectRepoFac.New()
+	prjs, err := projectRepo.GetAll(ctx)
+	if err != nil {
+		return []models.ProjectSpec{}, FromError(err, ProjectEntity, "")
+	}
+	return prjs, nil
 }
