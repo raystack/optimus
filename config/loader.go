9d43a574470c2e7e2c8036b7ce0d5092b147c2ed
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2022-03-22 19:36:13 +0700

    refactor: remove unecessary code

--- config/loader.go
+++ config/loader.go
@@ -45,16 +45,6 @@ func init() {
 	homePath = p
 }
 
-// MustLoadProjectConfig load the project specific config (see LoadProjectConfig) with panic
-func MustLoadProjectConfig(filepath ...string) *ProjectConfig {
-	cfg, err := LoadProjectConfig(filepath...)
-	if err != nil {
-		panic(err)
-	}
-
-	return cfg
-}
-
 // LoadProjectConfig load the project specific config from these locations:
 // 1. env var. eg. OPTIMUS_PROJECT, OPTIMUS_NAMESPACES, etc
 // 2. filepath. ./optimus <client_command> -c "path/to/config/optimus.yaml"
@@ -77,16 +67,6 @@ func loadProjectConfigFs(fs afero.Fs, filepath ...string) (*ProjectConfig, error
 	}
 
 	return cfg, nil
-}
-
-// MustLoadServerConfig load the server specific config (see LoadServerConfig) with panic
-func MustLoadServerConfig(filepath ...string) *ServerConfig {
-	cfg, err := LoadServerConfig(filepath...)
-	if err != nil {
-		panic(err)
-	}
-
-	return cfg
 }
 
 // LoadServerConfig load the server specific config from these locations:
@@ -178,51 +158,3 @@ func validateFilepath(fs afero.Fs, fpath string) error {
 	}
 	return nil
 }
-
-// type LoadConfigFunc func(interface{}, afero.Fs, ...string) error
-
-// // LoadOptimusConfig Load configuration file from following paths
-// // ./
-// // <exec>/
-// // ~/.optimus/
-// // Namespaces will be loaded only from current project ./
-// func LoadOptimusConfig(dirPaths ...string) (*Optimus, error) {
-// 	fs := afero.NewReadOnlyFs(afero.NewOsFs())
-
-// 	var targetPath string
-// 	if len(dirPaths) > 0 {
-// 		targetPath = dirPaths[0]
-// 	} else {
-// 		currPath, err := os.Getwd()
-// 		if err != nil {
-// 			return nil, fmt.Errorf("error getting current work directory path: %w", err)
-// 		}
-// 		targetPath = currPath
-// 	}
-
-// 	optimus := Optimus{}
-// 	if err := loadConfig(&optimus, fs, targetPath); err != nil {
-// 		return nil, errors.New("error loading config")
-// 	}
-// 	if err := validateNamespaceDuplication(&optimus); err != nil {
-// 		return nil, err
-// 	}
-// 	return &optimus, nil
-// }
-
-// func validateNamespaceDuplication(optimus *Optimus) error {
-// 	nameToAppearance := make(map[string]int)
-// 	for _, namespace := range optimus.Namespaces {
-// 		nameToAppearance[namespace.Name]++
-// 	}
-// 	var duplicateNames []string
-// 	for name, appearance := range nameToAppearance {
-// 		if appearance > 1 {
-// 			duplicateNames = append(duplicateNames, name)
-// 		}
-// 	}
-// 	if len(duplicateNames) > 0 {
-// 		return fmt.Errorf("namespaces [%s] are duplicate", strings.Join(duplicateNames, ", "))
-// 	}
-// 	return nil
-// }
