da6bda94a4fd14394b48bcf5384a8e44e2ea85c1
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2022-03-28 12:32:37 +0700

    fix: restore cli request fail

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	_ "net/http/pprof"
@@ -12,6 +13,8 @@ import (
 	"github.com/odpf/optimus/models"
 )
 
+var errRequestFail = errors.New("🔥 unable to complete request successfully")
+
 //nolint:forbidigo
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -22,7 +25,7 @@ func main() {
 	)
 
 	if err := command.Execute(); err != nil {
-		fmt.Println(err)
+		fmt.Println(errRequestFail)
 		os.Exit(1)
 	}
 }
