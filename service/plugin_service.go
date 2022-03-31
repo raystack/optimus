a393d5185e2e4572dd519fdf6ae5d2c252394140
Sandeep Bhardwaj <sandeep.bhardwaj@gojek.com>
2022-03-31 19:10:27 +0530

    feat: create plugin service to interact with plugins

---
+++ service/plugin_service.go
@@ -0,0 +1,103 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/odpf/optimus/compiler"
+	"github.com/odpf/optimus/models"
+)
+
+type PluginService interface {
+	GenerateDestination(context.Context, models.JobSpec, models.NamespaceSpec) (*models.GenerateDestinationResponse, error)
+	GenerateDependencies(context.Context, models.JobSpec, models.NamespaceSpec) (*models.GenerateDependenciesResponse, error)
+}
+
+type DependencyPluginService struct {
+	secretService SecretService
+	pluginRepo    models.PluginRepository
+	engine        models.TemplateEngine
+}
+
+func (d DependencyPluginService) GenerateDestination(ctx context.Context, jobSpec models.JobSpec, ns models.NamespaceSpec) (*models.GenerateDestinationResponse, error) {
+	plugin, err := d.pluginRepo.GetByName(jobSpec.Task.Unit.Info().Name)
+	if err != nil {
+		return nil, err
+	}
+
+	if plugin.DependencyMod == nil {
+		return nil, nil // TODO: decide based on caller to return error or nil
+	}
+
+	compiledConfigs, err := d.compileConfig(ctx, jobSpec.Task.Config, ns)
+	if err != nil {
+		return nil, err
+	}
+
+	destinationResp, err := plugin.DependencyMod.GenerateDestination(ctx, models.GenerateDestinationRequest{
+		Config:  compiledConfigs,
+		Assets:  models.PluginAssets{}.FromJobSpec(jobSpec.Assets),
+		Project: ns.ProjectSpec,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate destination: %w", err)
+	}
+
+	return destinationResp, nil
+}
+
+func (d DependencyPluginService) GenerateDependencies(ctx context.Context, jobSpec models.JobSpec, ns models.NamespaceSpec) (*models.GenerateDependenciesResponse, error) {
+	plugin, err := d.pluginRepo.GetByName(jobSpec.Task.Unit.Info().Name)
+	if err != nil {
+		return nil, err
+	}
+
+	if plugin.DependencyMod == nil {
+		return nil, nil
+	}
+
+	compiledConfigs, err := d.compileConfig(ctx, jobSpec.Task.Config, ns)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := plugin.DependencyMod.GenerateDependencies(ctx, models.GenerateDependenciesRequest{
+		Config:  compiledConfigs,
+		Assets:  models.PluginAssets{}.FromJobSpec(jobSpec.Assets),
+		Project: ns.ProjectSpec,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (d DependencyPluginService) compileConfig(ctx context.Context, configs models.JobSpecConfigs, namespace models.NamespaceSpec) (models.PluginConfigs, error) {
+	var secrets models.ProjectSecrets
+	var err error
+	if secrets, err = d.secretService.GetSecrets(ctx, namespace); err != nil {
+		return nil, err
+	}
+
+	tmplCtx := compiler.PrepareContext(
+		compiler.From(namespace.ProjectSpec.Config, namespace.Config).WithName("proj").WithKeyPrefix(compiler.ProjectConfigPrefix),
+		compiler.From(secrets.ToMap()).WithName("secret"),
+	)
+
+	for i, config := range configs {
+		compiledCnf, err := d.engine.CompileString(config.Value, tmplCtx)
+		if err == nil {
+			configs[i].Value = compiledCnf
+		}
+	}
+	return models.PluginConfigs{}.FromJobSpec(configs), nil
+}
+
+func NewPluginService(secretService SecretService, pluginRepo models.PluginRepository, engine models.TemplateEngine) *DependencyPluginService {
+	return &DependencyPluginService{
+		secretService: secretService,
+		pluginRepo:    pluginRepo,
+		engine:        engine,
+	}
+}
---
+++ service/plugin_service_test.go
@@ -0,0 +1,303 @@
+package service_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/odpf/optimus/compiler"
+	"github.com/odpf/optimus/mock"
+	"github.com/odpf/optimus/models"
+	"github.com/odpf/optimus/service"
+)
+
+func TestPluginService(t *testing.T) {
+	ctx := context.Background()
+	depMod := new(mock.DependencyResolverMod)
+	baseUnit := new(mock.BasePlugin)
+	plugin := &models.Plugin{Base: baseUnit, DependencyMod: depMod}
+	baseUnit.On("PluginInfo").Return(&models.PluginInfoResponse{
+		Name: "bq",
+	}, nil)
+
+	projectName := "a-data-project"
+	projectSpec := models.ProjectSpec{
+		Name: projectName,
+		Config: map[string]string{
+			"bucket": "gs://some_folder",
+		},
+	}
+
+	namespaceSpec := models.NamespaceSpec{
+		Name: "namespace-123",
+		Config: map[string]string{
+			"bucket": "gs://some_folder",
+		},
+		ProjectSpec: projectSpec,
+	}
+
+	secrets := []models.ProjectSecretItem{
+		{
+			ID:    uuid.New(),
+			Name:  "table_name",
+			Value: "secret_table",
+			Type:  models.SecretTypeUserDefined,
+		},
+		{
+			ID:    uuid.New(),
+			Name:  "bucket",
+			Value: "gs://some_secret_bucket",
+			Type:  models.SecretTypeUserDefined,
+		},
+	}
+	engine := compiler.NewGoEngine()
+
+	t.Run("GenerateDestination", func(t *testing.T) {
+		t.Run("return error when not able to get plugin", func(t *testing.T) {
+			pluginRepo := new(mock.SupportedPluginRepo)
+			pluginRepo.On("GetByName", "bq").Return(&models.Plugin{}, errors.New("plugin not found"))
+			defer pluginRepo.AssertExpectations(t)
+
+			jobSpec := models.JobSpec{
+				Version: 1,
+				Name:    "test1",
+				Task: models.JobSpecTask{
+					Unit: plugin,
+					Config: models.JobSpecConfigs{
+						{
+							Name:  "foo",
+							Value: "bar",
+						},
+					},
+				},
+			}
+
+			pluginService := service.NewPluginService(nil, pluginRepo, nil)
+			resp, err := pluginService.GenerateDestination(ctx, jobSpec, namespaceSpec)
+			assert.Nil(t, resp)
+			assert.EqualError(t, err, "plugin not found")
+		})
+		t.Run("return nil when not no dependency mod in plugin", func(t *testing.T) {
+			pluginRepo := new(mock.SupportedPluginRepo)
+			pluginRepo.On("GetByName", "bq").Return(&models.Plugin{Base: baseUnit}, nil)
+			defer pluginRepo.AssertExpectations(t)
+
+			jobSpec := models.JobSpec{
+				Version: 1,
+				Name:    "test1",
+				Task: models.JobSpecTask{
+					Unit: plugin,
+					Config: models.JobSpecConfigs{
+						{
+							Name:  "foo",
+							Value: "bar",
+						},
+					},
+				},
+			}
+
+			pluginService := service.NewPluginService(nil, pluginRepo, nil)
+			resp, err := pluginService.GenerateDestination(ctx, jobSpec, namespaceSpec)
+			assert.Nil(t, err)
+			assert.Nil(t, resp)
+		})
+		t.Run("return error when not not able to compile configs", func(t *testing.T) {
+			pluginRepo := new(mock.SupportedPluginRepo)
+			pluginRepo.On("GetByName", "bq").Return(plugin, nil)
+			defer pluginRepo.AssertExpectations(t)
+
+			secretService := new(mock.SecretService)
+			secretService.On("GetSecrets", ctx, namespaceSpec).Return([]models.ProjectSecretItem{}, errors.New("error"))
+			defer secretService.AssertExpectations(t)
+
+			jobSpec := models.JobSpec{
+				Version: 1,
+				Name:    "test1",
+				Task: models.JobSpecTask{
+					Unit: plugin,
+					Config: models.JobSpecConfigs{
+						{
+							Name:  "foo",
+							Value: "bar",
+						},
+					},
+				},
+			}
+
+			pluginService := service.NewPluginService(secretService, pluginRepo, engine)
+			resp, err := pluginService.GenerateDestination(ctx, jobSpec, namespaceSpec)
+			assert.Nil(t, resp)
+			assert.EqualError(t, err, "error")
+		})
+		t.Run("return destination successfully", func(t *testing.T) {
+			pluginRepo := new(mock.SupportedPluginRepo)
+			pluginRepo.On("GetByName", "bq").Return(plugin, nil)
+			defer pluginRepo.AssertExpectations(t)
+
+			secretService := new(mock.SecretService)
+			secretService.On("GetSecrets", ctx, namespaceSpec).Return(secrets, nil)
+			defer secretService.AssertExpectations(t)
+
+			jobSpec := models.JobSpec{
+				Version: 1,
+				Name:    "test1",
+				Task: models.JobSpecTask{
+					Unit: plugin,
+					Config: models.JobSpecConfigs{
+						{
+							Name:  "SECRET_TABLE_NAME",
+							Value: "{{.secret.table_name}}",
+						},
+					},
+				},
+			}
+
+			destReq := models.GenerateDestinationRequest{
+				Config: models.PluginConfigs{
+					{
+						Name:  "SECRET_TABLE_NAME",
+						Value: "secret_table",
+					},
+				},
+				Assets:  models.PluginAssets{},
+				Project: namespaceSpec.ProjectSpec,
+			}
+			depMod.On("GenerateDestination", ctx, destReq).Return(&models.GenerateDestinationResponse{
+				Destination: "project.dataset.table",
+				Type:        models.DestinationTypeBigquery,
+			}, nil)
+
+			pluginService := service.NewPluginService(secretService, pluginRepo, engine)
+			resp, err := pluginService.GenerateDestination(ctx, jobSpec, namespaceSpec)
+			assert.Nil(t, err)
+			assert.NotNil(t, resp)
+			assert.Equal(t, "project.dataset.table", resp.Destination)
+			assert.Equal(t, models.DestinationTypeBigquery, resp.Type)
+		})
+	})
+
+	t.Run("GenerateDependencies", func(t *testing.T) {
+		t.Run("return error when not able to get plugin", func(t *testing.T) {
+			pluginRepo := new(mock.SupportedPluginRepo)
+			pluginRepo.On("GetByName", "bq").Return(&models.Plugin{}, errors.New("plugin not found"))
+			defer pluginRepo.AssertExpectations(t)
+
+			jobSpec := models.JobSpec{
+				Version: 1,
+				Name:    "test1",
+				Task: models.JobSpecTask{
+					Unit: plugin,
+					Config: models.JobSpecConfigs{
+						{
+							Name:  "foo",
+							Value: "bar",
+						},
+					},
+				},
+			}
+
+			pluginService := service.NewPluginService(nil, pluginRepo, nil)
+			resp, err := pluginService.GenerateDependencies(ctx, jobSpec, namespaceSpec)
+			assert.Nil(t, resp)
+			assert.EqualError(t, err, "plugin not found")
+		})
+		t.Run("return nil when no dependency mod in plugin", func(t *testing.T) {
+			pluginRepo := new(mock.SupportedPluginRepo)
+			pluginRepo.On("GetByName", "bq").Return(&models.Plugin{Base: baseUnit}, nil)
+			defer pluginRepo.AssertExpectations(t)
+
+			jobSpec := models.JobSpec{
+				Version: 1,
+				Name:    "test1",
+				Task: models.JobSpecTask{
+					Unit: plugin,
+					Config: models.JobSpecConfigs{
+						{
+							Name:  "foo",
+							Value: "bar",
+						},
+					},
+				},
+			}
+
+			pluginService := service.NewPluginService(nil, pluginRepo, nil)
+			resp, err := pluginService.GenerateDependencies(ctx, jobSpec, namespaceSpec)
+			assert.Nil(t, err)
+			assert.Nil(t, resp)
+		})
+		t.Run("return error when not not able to compile configs", func(t *testing.T) {
+			pluginRepo := new(mock.SupportedPluginRepo)
+			pluginRepo.On("GetByName", "bq").Return(plugin, nil)
+			defer pluginRepo.AssertExpectations(t)
+
+			secretService := new(mock.SecretService)
+			secretService.On("GetSecrets", ctx, namespaceSpec).Return([]models.ProjectSecretItem{}, errors.New("error"))
+			defer secretService.AssertExpectations(t)
+
+			jobSpec := models.JobSpec{
+				Version: 1,
+				Name:    "test1",
+				Task: models.JobSpecTask{
+					Unit: plugin,
+					Config: models.JobSpecConfigs{
+						{
+							Name:  "foo",
+							Value: "bar",
+						},
+					},
+				},
+			}
+
+			pluginService := service.NewPluginService(secretService, pluginRepo, engine)
+			resp, err := pluginService.GenerateDependencies(ctx, jobSpec, namespaceSpec)
+			assert.Nil(t, resp)
+			assert.EqualError(t, err, "error")
+		})
+		t.Run("return dependencies successfully", func(t *testing.T) {
+			pluginRepo := new(mock.SupportedPluginRepo)
+			pluginRepo.On("GetByName", "bq").Return(plugin, nil)
+			defer pluginRepo.AssertExpectations(t)
+
+			secretService := new(mock.SecretService)
+			secretService.On("GetSecrets", ctx, namespaceSpec).Return(secrets, nil)
+			defer secretService.AssertExpectations(t)
+
+			jobSpec := models.JobSpec{
+				Version: 1,
+				Name:    "test1",
+				Task: models.JobSpecTask{
+					Unit: plugin,
+					Config: models.JobSpecConfigs{
+						{
+							Name:  "SECRET_TABLE_NAME",
+							Value: "{{.secret.table_name}}",
+						},
+					},
+				},
+			}
+
+			destReq := models.GenerateDependenciesRequest{
+				Config: models.PluginConfigs{
+					{
+						Name:  "SECRET_TABLE_NAME",
+						Value: "secret_table",
+					},
+				},
+				Assets:  models.PluginAssets{},
+				Project: namespaceSpec.ProjectSpec,
+			}
+			depMod.On("GenerateDependencies", ctx, destReq).Return(&models.GenerateDependenciesResponse{
+				Dependencies: []string{"project.dataset.table2_destination"}},
+				nil)
+
+			pluginService := service.NewPluginService(secretService, pluginRepo, engine)
+			resp, err := pluginService.GenerateDependencies(ctx, jobSpec, namespaceSpec)
+			assert.Nil(t, err)
+			assert.NotNil(t, resp)
+			assert.Equal(t, "project.dataset.table2_destination", resp.Dependencies[0])
+		})
+	})
+}
