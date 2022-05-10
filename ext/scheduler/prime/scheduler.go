861e37974619c985d4da0b74fd535eeb710f654b
Sandeep Bhardwaj <sandeep.bhardwaj@gojek.com>
2022-05-10 10:38:00 +0530

    fix: fix ci

--- ext/scheduler/prime/scheduler.go
+++ ext/scheduler/prime/scheduler.go
@@ -56,10 +56,9 @@ func (s *Scheduler) DeployJobsVerbose(ctx context.Context, namespace models.Name
 		})
 	}
 
-	repo := s.jobRunRepoFac.New()
 	jobDestination := "" // fetch job destination from plugin service
 	for _, runs := range jobRuns {
-		if err := repo.Save(ctx, namespace, runs, jobDestination); err != nil {
+		if err := s.jobRunRepo.Save(ctx, namespace, runs, jobDestination); err != nil {
 			return models.JobDeploymentDetail{}, err
 		}
 	}
