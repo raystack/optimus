b0d4c0ee572907edcba6a82170e69abd91924d2b
Anwar Hidayat <anwarhidayat14@gmail.com>
2022-05-11 13:49:15 +0700

    test: add unit test for checking path occupancy

---
+++ utils/file_test.go
@@ -0,0 +1,37 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/odpf/optimus/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsPathOccupied(t *testing.T) {
+	t.Run("should return false and nil if path does not exist", func(t *testing.T) {
+		path := "unknown"
+
+		actualExistence, actualErr := utils.IsPathOccupied(path)
+
+		assert.False(t, actualExistence)
+		assert.NoError(t, actualErr)
+	})
+
+	t.Run("should return false and error if unknown error encountered", func(t *testing.T) {
+		path := string([]byte{0})
+
+		actualExistence, actualErr := utils.IsPathOccupied(path)
+
+		assert.False(t, actualExistence)
+		assert.Error(t, actualErr)
+	})
+
+	t.Run("should return true and nil if path exist", func(t *testing.T) {
+		path := "file_test.go"
+
+		actualExistence, actualErr := utils.IsPathOccupied(path)
+
+		assert.True(t, actualExistence)
+		assert.NoError(t, actualErr)
+	})
+}
