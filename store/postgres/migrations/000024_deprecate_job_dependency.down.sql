01011c03a4f41152ba9d3dd6b50236f8ac815c52
Arinda Arif <arindaarif05@gmail.com>
2022-06-22 16:42:29 +0700

    fix: change migration down script for dropping job_dependency table
    Co-authored-by: Anwar Hidayat <anwarhidayat14@gmail.com>

--- store/postgres/migrations/000024_deprecate_job_dependency.down.sql
+++ store/postgres/migrations/000024_deprecate_job_dependency.down.sql
@@ -1,1 +1,9 @@
-DROP TABLE IF EXISTS job_dependency;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS job_dependency (
+    job_id UUID NOT NULL,
+    project_id UUID NOT NULL REFERENCES project (id),
+    dependent_job_id UUID NOT NULL,
+    dependent_project_id UUID NOT NULL REFERENCES project (id),
+    type VARCHAR(10),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
