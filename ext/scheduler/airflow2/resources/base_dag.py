80aab87985ee7e6d861ca39d46c3764e589806c8
Yash Bhardwaj <imyashbhardwaj@gmail.com>
2022-06-29 14:10:24 +0530

    fix: use job_spec api to get schedule interval for sensors

--- ext/scheduler/airflow2/resources/base_dag.py
+++ ext/scheduler/airflow2/resources/base_dag.py
@@ -191,6 +191,7 @@ hook_{{$hookSchema.Name | replace "-" "__dash__"}} = SuperKubernetesPodOperator(
 wait_{{$dependency.Job.Name | replace "-" "__dash__" | replace "." "__dot__"}} = SuperExternalTaskSensor(
     optimus_hostname="{{$.Hostname}}",
     upstream_optimus_project="{{$.Namespace.ProjectSpec.Name}}",
+    upstream_optimus_namespace="{{$.Namespace.Name}}",
     upstream_optimus_job="{{$dependency.Job.Name}}",
     window_size="{{ $baseWindow.Size.String }}",
     poke_interval=SENSOR_DEFAULT_POKE_INTERVAL_IN_SECS,
