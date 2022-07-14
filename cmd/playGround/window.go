b3847be4b54978bc370aeb35ead3d3f550238c91
Tharun Reddy <f20190190@hyderabad.bits-pilani.ac.in>
2022-07-14 12:07:17 +0530

    modified the windowing logic and tests

--- cmd/playGround/window.go
+++ cmd/playGround/window.go
@@ -1,4 +1,5 @@
 package playground
+
 // contins the main window logic
 import (
 	"fmt"
@@ -11,25 +12,25 @@ type Window struct {
 	survey survey.WindowSurvey
 }
 
-// logic:- we trucate hour its its hour 
-//we trucate both hour,day for day 
+// logic:- we trucate hour its its hour
+//we trucate both hour,day for day
 //we trucate hour,day,week for week
 //we trucate both hour,day,month for month
 func (j *Window) truncate(currentFinishingDate time.Time, truncated string) time.Time {
 	//add the remaining minitues to the nearest hour to round it up
 	if currentFinishingDate.Minute() != 0 {
-		k := (60 - currentFinishingDate.Minute())
+		k := currentFinishingDate.Minute()
 		remainingMinituesToTruncate := time.Duration(k) * (time.Minute)
-		currentFinishingDate = currentFinishingDate.Add(remainingMinituesToTruncate)
+		currentFinishingDate = currentFinishingDate.Add(-1 * remainingMinituesToTruncate)
 	}
 	if truncated == "hour" {
 		return currentFinishingDate
 	}
 	//add the remaining hours to the nearest day to round it up
 	if currentFinishingDate.Hour() != 0 {
-		k := 24 - currentFinishingDate.Hour()
+		k := currentFinishingDate.Hour()
 		remainingHoursToTruncate := time.Duration(k) * (time.Hour)
-		currentFinishingDate = currentFinishingDate.Add(remainingHoursToTruncate)
+		currentFinishingDate = currentFinishingDate.Add(-1 * remainingHoursToTruncate)
 	}
 	if truncated == "day" {
 		return currentFinishingDate
@@ -37,29 +38,25 @@ func (j *Window) truncate(currentFinishingDate time.Time, truncated string) time
 	// since we round for both day and hour we are sure that the time is 00:00 so we only have to grt to the nearest monday
 	if truncated == "week" {
 		if int(currentFinishingDate.Weekday()) != 1 {
-			k := 8 - int(currentFinishingDate.Weekday())
-			if k == 8 {
-				k = 1
+			k := int(currentFinishingDate.Weekday())
+			if k == 0 {
+				k = 7
 			}
+			k--
 			remainingHoursToTruncate := time.Duration(k*24) * (time.Hour)
-			currentFinishingDate = currentFinishingDate.Add(remainingHoursToTruncate)
+			currentFinishingDate = currentFinishingDate.Add(-1 * remainingHoursToTruncate)
 		}
 		return currentFinishingDate
 	}
-	// we use the same logic as the week here excep we round uo to 1 st of every month
-	if currentFinishingDate.Day() != 1 {
-		currentFinishingDate = currentFinishingDate.AddDate(0, 1, 0)
-	}
-	currentFinishingDate = currentFinishingDate.AddDate(0, 0, -currentFinishingDate.Day()+1)
-	return currentFinishingDate
-	// note that here although we write 1st 00:00 it can also be treated as end of last month as 31st 24:00
+	return time.Date(currentFinishingDate.Year(), currentFinishingDate.Month(), 1, 0, 0, 0, 0, time.UTC)
 }
 
 // using add date for offset and calculating the original window
-func (j *Window) applyoffset(currentFinishingDate time.Time, offset int) time.Time {
-	currentFinishingDate = currentFinishingDate.Add(time.Duration(-1*offset) * time.Hour)
+func (j *Window) applyoffset(currentFinishingDate time.Time, offset MonthHour) time.Time {
+	currentFinishingDate = currentFinishingDate.AddDate(0, offset.month, 0).Add(time.Duration(offset.hour) * time.Hour)
 	return currentFinishingDate
 }
+
 // printing the results
 func (j *Window) printWindow(currentFinishingDate time.Time, size int) {
 	dStart := currentFinishingDate.Add(time.Duration(-1*size) * time.Hour)
