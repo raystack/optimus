7fe5c49babfe2f5a2e42eca44c182c903ee2329e
Anwar Hidayat <anwarhidayat14@gmail.com>
2022-07-15 10:52:13 +0700

    refactor: move out new window to a separate file

---
+++ job/window.go
@@ -0,0 +1,21 @@
+package job
+
+import (
+	"fmt"
+	"time"
+)
+
+type Window interface {
+	GetTimeRange(scheduleTime time.Time) (time.Time, time.Time, error)
+	Validate() error
+}
+
+func NewWindow(version int, truncateTo, offset, size string) (Window, error) {
+	if version == 1 {
+		return WindowV1{size, offset, truncateTo}, nil
+	}
+	if version == 2 {
+		return WindowV2{size, offset, truncateTo}, nil
+	}
+	return nil, fmt.Errorf("window version [%d] is not recognized", version)
+}
