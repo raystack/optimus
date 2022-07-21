ab3c38eeb41b799a99eca0dbea9e528adcc56cd4
Yash Bhardwaj <imyashbhardwaj@gmail.com>
2022-07-21 18:53:14 +0530

    fix: capture scheduler exception errors in optimus notifications

--- job/event.go
+++ job/event.go
@@ -34,6 +34,7 @@ func (e *eventService) Register(ctx context.Context, namespace models.NamespaceS
 	evt models.JobEvent) error {
 	var err error
 	for _, notify := range jobSpec.Behavior.Notify {
+		//bug here: with new notification types, this wont work
 		if notify.On == evt.Type {
 			for _, channel := range notify.Channels {
 				chanParts := strings.Split(channel, "://")
