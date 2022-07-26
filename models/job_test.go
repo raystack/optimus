20ec29f1263d5942406a1fb2fc562204a5bda427
Anwar Hidayat <anwarhidayat14@gmail.com>
2022-07-26 14:54:54 +0700

    test: update unit test to pass the old implementation
    Note: these unit tests might not make sense at some point, as the old implementation is buggy.
    The old version is retained as much as possible, to allow for short-term backward compatibility.
    Later, old version (or in this case version 1) will be deprecated

--- models/job_test.go
+++ models/job_test.go
@@ -2,7 +2,6 @@ package models_test
 
 import (
 	"testing"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -28,164 +27,6 @@ func TestJob(t *testing.T) {
 			},
 		}
 		assert.Equal(t, "project-name/job-name", jobSpec.GetFullName())
-	})
-	t.Run("JobSpecTaskWindow", func(t *testing.T) {
-		t.Run("should generate valid window start and end", func(t *testing.T) {
-			cases := []struct {
-				Today              time.Time
-				WindowSize         time.Duration
-				WindowOffset       time.Duration
-				WindowTruncateUpto string
-
-				ExpectedStart time.Time
-				ExpectedEnd   time.Time
-			}{
-				{
-					Today:              time.Date(2021, 2, 25, 0, 0, 0, 0, time.UTC),
-					WindowSize:         24 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "",
-					ExpectedStart:      time.Date(2021, 2, 24, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2021, 2, 25, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "",
-					ExpectedStart:      time.Date(2020, 7, 9, 6, 33, 22, 0, time.UTC),
-					ExpectedEnd:        time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "h",
-					ExpectedStart:      time.Date(2020, 7, 9, 6, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2020, 7, 10, 6, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "d",
-					ExpectedStart:      time.Date(2020, 7, 9, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2020, 7, 10, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
-					WindowSize:         48 * time.Hour,
-					WindowOffset:       24 * time.Hour,
-					WindowTruncateUpto: "d",
-					ExpectedStart:      time.Date(2020, 7, 9, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2020, 7, 11, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * time.Hour,
-					WindowOffset:       -24 * time.Hour,
-					WindowTruncateUpto: "d",
-					ExpectedStart:      time.Date(2020, 7, 8, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2020, 7, 9, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * 7 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "w",
-					ExpectedStart:      time.Date(2020, 7, 5, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2020, 7, 12, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2020, 7, 12, 0, 0, 0, 0, time.UTC),
-					WindowSize:         24 * 7 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "w",
-					ExpectedStart:      time.Date(2020, 7, 12, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2020, 7, 19, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "M",
-					ExpectedStart:      time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * 30 * time.Hour,
-					WindowOffset:       24 * 32 * time.Hour,
-					WindowTruncateUpto: "M",
-					ExpectedStart:      time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2021, 3, 31, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2021, 2, 01, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * 62 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "M",
-					ExpectedStart:      time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2021, 2, 28, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * 30 * time.Hour,
-					WindowOffset:       24 * 30 * time.Hour,
-					WindowTruncateUpto: "M",
-					ExpectedStart:      time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2021, 3, 31, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * 30 * time.Hour,
-					WindowOffset:       -24 * 30 * time.Hour,
-					WindowTruncateUpto: "M",
-					ExpectedStart:      time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2021, 1, 31, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * 20 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "M",
-					ExpectedStart:      time.Date(2021, 2, 1, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2021, 2, 28, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * 60 * time.Hour,
-					WindowOffset:       0,
-					WindowTruncateUpto: "M",
-					ExpectedStart:      time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2021, 2, 28, 0, 0, 0, 0, time.UTC),
-				},
-				{
-					Today:              time.Date(2021, 3, 31, 6, 33, 22, 0, time.UTC),
-					WindowSize:         24 * 30 * time.Hour,
-					WindowOffset:       -24 * 30 * time.Hour,
-					WindowTruncateUpto: "M",
-					ExpectedStart:      time.Date(2021, 2, 1, 0, 0, 0, 0, time.UTC),
-					ExpectedEnd:        time.Date(2021, 2, 28, 0, 0, 0, 0, time.UTC),
-				},
-			}
-
-			for _, tcase := range cases {
-				win := &&models.WindowV1{
-					SizeAsDuration:   tcase.WindowSize,
-					OffsetAsDuration: tcase.WindowOffset,
-					TruncateTo:       tcase.WindowTruncateUpto,
-				}
-
-				actualStartTime, actualStartError := win.GetStartTime(tcase.Today)
-				actualEndTime, actualEndError := win.GetEndTime(tcase.Today)
-
-				assert.Equal(t, tcase.ExpectedStart.String(), actualStartTime.String())
-				assert.NoError(t, actualStartError)
-				assert.Equal(t, tcase.ExpectedEnd.String(), actualEndTime.String())
-				assert.NoError(t, actualEndError)
-			}
-		})
 	})
 	t.Run("GetJobDependencyMap", func(t *testing.T) {
 		t.Run("should able to create a map of job ID and its dependencies", func(t *testing.T) {
@@ -333,6 +174,10 @@ func TestJob(t *testing.T) {
 	})
 	t.Run("SLADuration", func(t *testing.T) {
 		t.Run("should able to get defined SLA duration", func(t *testing.T) {
+			window, err := models.NewWindow(1, "d", "0", "1h")
+			if err != nil {
+				panic(err)
+			}
 			jobSpecs := models.JobSpec{
 				Name:  "foo",
 				Owner: "mee@mee",
@@ -350,11 +195,7 @@ func TestJob(t *testing.T) {
 				Task: models.JobSpecTask{
 					Unit:     &models.Plugin{},
 					Priority: 2000,
-					Window: &&models.WindowV1{
-						SizeAsDuration:   time.Hour,
-						OffsetAsDuration: 0,
-						TruncateTo:       "d",
-					},
+					Window:   window,
 				},
 			}
 
--- models/window_v1_test.go
+++ models/window_v1_test.go
@@ -3,6 +3,7 @@ package models_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -42,6 +43,163 @@ func TestWindowV1(t *testing.T) {
 				}
 				err = window.Validate()
 				assert.Nil(t, err, fmt.Sprintf("failed for : %s", config))
+			}
+		})
+	})
+	t.Run("GetTimeRange", func(t *testing.T) {
+		t.Run("should provide start and end time window for the given schedule", func(t *testing.T) {
+			cases := []struct {
+				ScheduleTime      time.Time
+				Size              string
+				Offset            string
+				TruncateTo        string
+				ExpectedStartTime time.Time
+				ExpectedEndTime   time.Time
+			}{
+				{
+					ScheduleTime:      time.Date(2021, 2, 25, 0, 0, 0, 0, time.UTC),
+					Size:              "24h",
+					Offset:            "0",
+					TruncateTo:        "",
+					ExpectedStartTime: time.Date(2021, 2, 24, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2021, 2, 25, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
+					Size:              "24h",
+					Offset:            "0",
+					TruncateTo:        "",
+					ExpectedStartTime: time.Date(2020, 7, 9, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2020, 7, 10, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
+					Size:              "24h",
+					Offset:            "0",
+					TruncateTo:        "h",
+					ExpectedStartTime: time.Date(2020, 7, 9, 6, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2020, 7, 10, 6, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
+					Size:              "24h",
+					Offset:            "0",
+					TruncateTo:        "d",
+					ExpectedStartTime: time.Date(2020, 7, 9, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2020, 7, 10, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
+					Size:              "48h",
+					Offset:            "24h",
+					TruncateTo:        "d",
+					ExpectedStartTime: time.Date(2020, 7, 9, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2020, 7, 11, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
+					Size:              "24h",
+					Offset:            "-24h",
+					TruncateTo:        "d",
+					ExpectedStartTime: time.Date(2020, 7, 8, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2020, 7, 9, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2020, 7, 10, 6, 33, 22, 0, time.UTC),
+					Size:              "168h",
+					Offset:            "0",
+					TruncateTo:        "w",
+					ExpectedStartTime: time.Date(2020, 7, 5, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2020, 7, 12, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2020, 7, 12, 0, 0, 0, 0, time.UTC),
+					Size:              "168h",
+					Offset:            "0",
+					TruncateTo:        "w",
+					ExpectedStartTime: time.Date(2020, 7, 12, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2020, 7, 19, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
+					Size:              "24h",
+					Offset:            "0",
+					TruncateTo:        "M",
+					ExpectedStartTime: time.Date(2021, 2, 1, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2021, 2, 28, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
+					Size:              "720h",
+					Offset:            "768h",
+					TruncateTo:        "M",
+					ExpectedStartTime: time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2021, 3, 31, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2021, 2, 01, 6, 33, 22, 0, time.UTC),
+					Size:              "1488h",
+					Offset:            "0",
+					TruncateTo:        "M",
+					ExpectedStartTime: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2021, 2, 28, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
+					Size:              "720h",
+					Offset:            "720h",
+					TruncateTo:        "M",
+					ExpectedStartTime: time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2021, 3, 31, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
+					Size:              "720h",
+					Offset:            "-720h",
+					TruncateTo:        "M",
+					ExpectedStartTime: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2021, 1, 31, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
+					Size:              "480h",
+					Offset:            "0",
+					TruncateTo:        "M",
+					ExpectedStartTime: time.Date(2021, 2, 1, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2021, 2, 28, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2021, 2, 25, 6, 33, 22, 0, time.UTC),
+					Size:              "1440h",
+					Offset:            "0",
+					TruncateTo:        "M",
+					ExpectedStartTime: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2021, 2, 28, 0, 0, 0, 0, time.UTC),
+				},
+				{
+					ScheduleTime:      time.Date(2021, 3, 31, 6, 33, 22, 0, time.UTC),
+					Size:              "720h",
+					Offset:            "-720h",
+					TruncateTo:        "M",
+					ExpectedStartTime: time.Date(2021, 2, 1, 0, 0, 0, 0, time.UTC),
+					ExpectedEndTime:   time.Date(2021, 2, 28, 0, 0, 0, 0, time.UTC),
+				},
+			}
+			for _, sc := range cases {
+				w, err := models.NewWindow(1, sc.TruncateTo, sc.Offset, sc.Size)
+				if err != nil {
+					panic(err)
+				}
+
+				actualValidateError := w.Validate()
+				actualStartTime, actualStartTimeError := w.GetStartTime(sc.ScheduleTime)
+				actualEndTime, actualEndTimeError := w.GetEndTime(sc.ScheduleTime)
+
+				assert.NoError(t, actualValidateError)
+				assert.Equal(t, sc.ExpectedStartTime.String(), actualStartTime.String())
+				assert.NoError(t, actualStartTimeError)
+				assert.Equal(t, sc.ExpectedEndTime.String(), actualEndTime.String())
+				assert.NoError(t, actualEndTimeError)
 			}
 		})
 	})
