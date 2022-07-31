2edbafec9999befcdb3b51a2f23b439f5414aa4d
Saikumar Chintada <chintadasaikumar@gmail.com>
2022-07-31 20:54:55 +0530

    feat : add plugin manager (#434)
    * add plugin manager
    
    * fix : change flag to --install-plugins
    
    * fix : cosmetic changes
    
    * fix : make current dir default for plugins
    
    * fix : config test and remove comments
    
    * fix : review feedback
    
    * fix go mod
    
    * minor fix

--- cmd/commands.go
+++ cmd/commands.go
@@ -16,6 +16,7 @@ import (
 	"github.com/odpf/optimus/cmd/job"
 	"github.com/odpf/optimus/cmd/logger"
 	"github.com/odpf/optimus/cmd/namespace"
+	"github.com/odpf/optimus/cmd/plugin"
 	"github.com/odpf/optimus/cmd/project"
 	"github.com/odpf/optimus/cmd/replay"
 	"github.com/odpf/optimus/cmd/resource"
@@ -79,9 +80,11 @@ func New() *cli.Command {
 		secret.NewSecretCommand(),
 		version.NewVersionCommand(),
 	)
-
 	// Server related commands
-	cmd.AddCommand(serve.NewServeCommand())
+	cmd.AddCommand(
+		serve.NewServeCommand(),
+		plugin.NewPluginCommand(),
+	)
 
 	extension.UpdateWithExtension(cmd)
 	return cmd
