970cfe52d8c4685c94ca147c0197ce8929e27184
Tharun Reddy <f20190190@hyderabad.bits-pilani.ac.in>
2022-08-24 12:20:39 +0530

    fix: changes done according to feedback

--- cmd/internal/logger/color.go
+++ cmd/internal/logger/color.go
@@ -7,10 +7,6 @@ import (
 )
 
 var (
-	// ColoredError format message with color for error
-	ColoredError = fmt.Sprintf
-	// ColoredNotice format message with color for notification
-	ColoredNotice = fmt.Sprintf
 	// ColoredSuccess format message with color for success
 	ColoredSuccess = fmt.Sprintf
 
