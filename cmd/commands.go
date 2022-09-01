b9db17242f1d4d177c07a1b53ae4ae2101d8aed6
Anwar Hidayat <anwarhidayat14@gmail.com>
2022-09-01 10:50:38 +0700

    feat: add command for migration rollback

--- cmd/commands.go
+++ cmd/commands.go
@@ -14,6 +14,7 @@ import (
 	"github.com/odpf/optimus/cmd/initialize"
 	"github.com/odpf/optimus/cmd/internal/logger"
 	"github.com/odpf/optimus/cmd/job"
+	"github.com/odpf/optimus/cmd/migration"
 	"github.com/odpf/optimus/cmd/namespace"
 	"github.com/odpf/optimus/cmd/plugin"
 	"github.com/odpf/optimus/cmd/project"
@@ -72,6 +73,7 @@ func New() *cli.Command {
 		deploy.NewDeployCommand(),
 		initialize.NewInitializeCommand(),
 		job.NewJobCommand(),
+		migration.NewMigrationCommand(),
 		namespace.NewNamespaceCommand(),
 		project.NewProjectCommand(),
 		replay.NewReplayCommand(),
