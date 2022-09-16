d83b6ed05635173ec82492a99eff95d41bc15837
Sandeep Bhardwaj <sandeep.bhardwaj@gojek.com>
2022-09-16 12:24:51 +0530

    fix: restore secrets fallback (#604)

--- plugin/v1beta1/cli/client.go
+++ plugin/v1beta1/cli/client.go
@@ -105,6 +105,9 @@ func (m *GRPCClient) DefaultAssets(ctx context.Context, request models.DefaultAs
 }
 
 func (m *GRPCClient) CompileAssets(ctx context.Context, request models.CompileAssetsRequest) (*models.CompileAssetsResponse, error) {
+	_, span := base.Tracer.Start(ctx, "CompileAssets")
+	defer span.End()
+
 	var instanceData []*pb.InstanceSpecData
 	for _, inst := range request.InstanceData {
 		instanceData = append(instanceData, &pb.InstanceSpecData{
