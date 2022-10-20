22a113e38e473ca97f494ca9f9f76431dda2da0f
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2022-10-20 18:28:32 +0700

    refactor: remove commandline mod

--- plugin/yaml/plugin.go
+++ plugin/yaml/plugin.go
@@ -32,7 +32,7 @@ func (p *PluginSpec) PluginInfo() (*models.PluginInfoResponse, error) { // nolin
 		Image:         fmt.Sprintf("%s:%s", p.Image, p.PluginVersion),
 		SecretPath:    p.SecretPath,
 		PluginType:    p.PluginType,
-		PluginMods:    []models.PluginMod{models.ModTypeCLI},
+		PluginMods:    []models.PluginMod{models.ModTypeYaml},
 		PluginVersion: p.PluginVersion,
 		HookType:      p.HookType,
 		DependsOn:     p.DependsOn,
