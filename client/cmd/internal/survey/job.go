c589d19cd72446fe749dcce54315a44ab0a4a7e1
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2022-10-20 18:28:32 +0700

    refactor: remove commandline mod

--- client/cmd/internal/survey/job.go
+++ client/cmd/internal/survey/job.go
@@ -51,14 +51,14 @@ func (*JobSurvey) AskToSelectJobName(jobSpecReader local.SpecReader[*model.JobSp
 	return selectedJobName, nil
 }
 
-func (j *JobSurvey) askCLIModSurveyQuestion(ctx context.Context, cliMod models.CommandLineMod, question models.PluginQuestion) (models.PluginAnswers, error) {
+func (j *JobSurvey) askYamlModSurveyQuestion(ctx context.Context, yamlMod models.YamlMod, question models.PluginQuestion) (models.PluginAnswers, error) {
 	surveyPrompt := j.getSurveyPromptFromPluginQuestion(question)
 
 	var responseStr string
 	if err := survey.AskOne(
 		surveyPrompt,
 		&responseStr,
-		survey.WithValidator(j.getValidatePluginQuestion(ctx, cliMod, question)),
+		survey.WithValidator(j.getValidatePluginQuestion(ctx, yamlMod, question)),
 	); err != nil {
 		return nil, fmt.Errorf("AskSurveyQuestion: %w", err)
 	}
@@ -74,7 +74,7 @@ func (j *JobSurvey) askCLIModSurveyQuestion(ctx context.Context, cliMod models.C
 	for _, subQues := range question.SubQuestions {
 		if responseStr == subQues.IfValue {
 			for _, subQuestion := range subQues.Questions {
-				subQuestionAnswers, err := j.askCLIModSurveyQuestion(ctx, cliMod, subQuestion)
+				subQuestionAnswers, err := j.askYamlModSurveyQuestion(ctx, yamlMod, subQuestion)
 				if err != nil {
 					return nil, err
 				}
@@ -111,13 +111,13 @@ func (*JobSurvey) getSurveyPromptFromPluginQuestion(question models.PluginQuesti
 	return surveyPrompt
 }
 
-func (j *JobSurvey) getValidatePluginQuestion(ctx context.Context, cliMod models.CommandLineMod, question models.PluginQuestion) survey.Validator {
+func (j *JobSurvey) getValidatePluginQuestion(ctx context.Context, yamlMod models.YamlMod, question models.PluginQuestion) survey.Validator {
 	return func(val interface{}) error {
 		str, err := j.convertUserInputPluginToString(val)
 		if err != nil {
 			return err
 		}
-		resp, err := cliMod.ValidateQuestion(ctx, models.ValidateQuestionRequest{
+		resp, err := yamlMod.ValidateQuestion(ctx, models.ValidateQuestionRequest{
 			Answer: models.PluginAnswer{
 				Question: question,
 				Value:    str,
