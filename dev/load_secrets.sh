495d3eb6b4d8b96c0dded50c869030ce56023e78
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2022-11-03 19:13:15 +0700

    feat: load secret from file contains key value

--- dev/load_secrets.sh
+++ dev/load_secrets.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 
+OPTIMUS_NAMESPACE=${NAMESPACE:-optimus-dev}
 PROJECT=${PROJECT:-project-a}
-HOST=${HOST:-localhost:9100}
-SECRETS_PATH=$1
+HOST=${HOST:-"localhost:9100"}
+SETUP_PATH=$1
 
-if [ -z "$SECRETS_PATH" ]; then
-  >&2 echo "must provide secrets_path:  ./load_secret <secrets_path>"
+if [ -z "$SETUP_PATH" ]; then
+  >&2 echo "must provide setup_path: ./load_secrets <secrets_path>"
   exit 1
 fi
 
-while read line || [[ -n $line ]]; do
-  echo $line | awk '{sub(/=/," ")}1' | sed 's/\"//g;' | while read -r key value; do
-    curl -XPOST -H "Content-Type: application/json" \
+# load secret
+
+echo "load secrets into project ${PROJECT}"
+if [ -z $(command -v yq) ]; then
+  >&2 echo "yq must be installed: \`brew install yq\`"
+  exit 1
+fi
+if ! curl --output /dev/null --silent ${HOST}/ping; then
+  >&2 echo "can't connect to optimus host ${HOST}"
+  exit 1
+fi
+
+yq '.secrets[] | (.name | . + " ") + (.value | @base64)' ${SETUP_PATH} >> secret_decoded.yaml
+while read key value; do
+    curl -XPUT -H "Content-Type: application/json" \
     "${HOST}/api/v1beta1/project/${PROJECT}/secret/${key}" \
-    -d '{"value": "'${val}'"}'
-  done
-done < $SECRETS_PATH
+    -d '{"value": "'${value}'"}' -s -o /dev/null -w "set secret $key: %{http_code}\n"
+done < secret_decoded.yaml
+rm secret_decoded.yaml
