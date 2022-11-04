168debe2ea2f62bb8f518e92a318afb2685661d2
Anwar Hidayat <anwarhidayat14@gmail.com>
2022-11-04 15:46:37 +0700

    refactor: Revert "refactor: move out job spec implementations to different files"
    This reverts commit f6c0c638c0fdb01bc37201e0d05d2a64a556bfbd.

--- core/job/spec.go
+++ core/job/spec.go
@@ -145,3 +145,252 @@ func (w Window) Offset() string {
 func (w Window) TruncateTo() string {
 	return w.truncateTo
 }
+
+type Config struct {
+	config map[string]string
+}
+
+func (c Config) Config() map[string]string {
+	return c.config
+}
+
+func NewConfig(config map[string]string) *Config {
+	return &Config{config: config}
+}
+
+type Task struct {
+	name   string
+	config *Config
+}
+
+func (t Task) Name() string {
+	return t.name
+}
+
+func (t Task) Config() *Config {
+	return t.config
+}
+
+func NewTask(name string, config *Config) *Task {
+	return &Task{name: name, config: config}
+}
+
+type Hook struct {
+	name   string
+	config *Config
+}
+
+func (h Hook) Name() string {
+	return h.name
+}
+
+func (h Hook) Config() *Config {
+	return h.config
+}
+
+func NewHook(name string, config *Config) *Hook {
+	return &Hook{name: name, config: config}
+}
+
+type SpecUpstream struct {
+	upstreamNames []string
+	httpUpstreams []*HTTPUpstreams
+}
+
+func NewSpecUpstream(upstreamNames []string, httpUpstreams []*HTTPUpstreams) *SpecUpstream {
+	return &SpecUpstream{upstreamNames: upstreamNames, httpUpstreams: httpUpstreams}
+}
+
+func (s SpecUpstream) UpstreamNames() []string {
+	return s.upstreamNames
+}
+
+func (s SpecUpstream) HTTPUpstreams() []*HTTPUpstreams {
+	return s.httpUpstreams
+}
+
+type HTTPUpstreams struct {
+	name    string
+	url     string
+	headers map[string]string
+	params  map[string]string
+}
+
+func (h HTTPUpstreams) Name() string {
+	return h.name
+}
+
+func (h HTTPUpstreams) URL() string {
+	return h.url
+}
+
+func (h HTTPUpstreams) Headers() map[string]string {
+	return h.headers
+}
+
+func (h HTTPUpstreams) Params() map[string]string {
+	return h.params
+}
+
+func NewHTTPUpstream(name string, url string, headers map[string]string, params map[string]string) *HTTPUpstreams {
+	return &HTTPUpstreams{name: name, url: url, headers: headers, params: params}
+}
+
+type Schedule struct {
+	startDate     string // to check
+	endDate       string // to check
+	interval      string // to check
+	dependsOnPast bool
+	catchUp       bool
+	retry         *Retry
+}
+
+func (s Schedule) StartDate() string {
+	return s.startDate
+}
+
+func (s Schedule) EndDate() string {
+	return s.endDate
+}
+
+func (s Schedule) Interval() string {
+	return s.interval
+}
+
+func (s Schedule) DependsOnPast() bool {
+	return s.dependsOnPast
+}
+
+func (s Schedule) CatchUp() bool {
+	return s.catchUp
+}
+
+func (s Schedule) Retry() *Retry {
+	return s.retry
+}
+
+func NewSchedule(startDate string, endDate string, interval string, dependsOnPast bool, catchUp bool, retry *Retry) *Schedule {
+	return &Schedule{startDate: startDate, endDate: endDate, interval: interval, dependsOnPast: dependsOnPast, catchUp: catchUp, retry: retry}
+}
+
+type Retry struct {
+	count              int
+	delay              int32
+	exponentialBackoff bool
+}
+
+func (r Retry) Count() int {
+	return r.count
+}
+
+func (r Retry) Delay() int32 {
+	return r.delay
+}
+
+func (r Retry) ExponentialBackoff() bool {
+	return r.exponentialBackoff
+}
+
+func NewRetry(count int, delay int32, exponentialBackoff bool) *Retry {
+	return &Retry{count: count, delay: delay, exponentialBackoff: exponentialBackoff}
+}
+
+type Alert struct {
+	on       EventType
+	channels []string
+	config   map[string]string
+}
+
+func (a Alert) On() EventType {
+	return a.on
+}
+
+func (a Alert) Channels() []string {
+	return a.channels
+}
+
+func (a Alert) Config() map[string]string {
+	return a.config
+}
+
+func NewAlert(on EventType, channels []string, config map[string]string) *Alert {
+	return &Alert{on: on, channels: channels, config: config}
+}
+
+type EventType string
+
+const (
+	SLAMissEvent EventType = "sla_miss"
+
+	JobFailureEvent EventType = "failure"
+	JobStartEvent   EventType = "job_start"
+	JobFailEvent    EventType = "job_fail"
+	JobSuccessEvent EventType = "job_success"
+	JobRetryEvent   EventType = "retry"
+
+	TaskStartEvent   EventType = "task_start"
+	TaskRetryEvent   EventType = "task_retry"
+	TaskFailEvent    EventType = "task_fail"
+	TaskSuccessEvent EventType = "task_success"
+
+	HookStartEvent   EventType = "hook_start"
+	HookRetryEvent   EventType = "hook_retry"
+	HookFailEvent    EventType = "hook_fail"
+	HookSuccessEvent EventType = "hook_success"
+
+	SensorStartEvent   EventType = "sensor_start"
+	SensorRetryEvent   EventType = "sensor_retry"
+	SensorFailEvent    EventType = "sensor_fail"
+	SensorSuccessEvent EventType = "sensor_success"
+)
+
+type Metadata struct {
+	resource  *ResourceMetadata
+	scheduler map[string]string
+}
+
+func (m Metadata) Resource() *ResourceMetadata {
+	return m.resource
+}
+
+func (m Metadata) Scheduler() map[string]string {
+	return m.scheduler
+}
+
+func NewMetadata(resource *ResourceMetadata, scheduler map[string]string) *Metadata {
+	return &Metadata{resource: resource, scheduler: scheduler}
+}
+
+type ResourceMetadata struct {
+	request *ResourceConfig
+	limit   *ResourceConfig
+}
+
+func (r ResourceMetadata) Request() *ResourceConfig {
+	return r.request
+}
+
+func (r ResourceMetadata) Limit() *ResourceConfig {
+	return r.limit
+}
+
+func NewResourceMetadata(request *ResourceConfig, limit *ResourceConfig) *ResourceMetadata {
+	return &ResourceMetadata{request: request, limit: limit}
+}
+
+type ResourceConfig struct {
+	cpu    string
+	memory string
+}
+
+func (r ResourceConfig) CPU() string {
+	return r.cpu
+}
+
+func (r ResourceConfig) Memory() string {
+	return r.memory
+}
+
+func NewResourceConfig(cpu string, memory string) *ResourceConfig {
+	return &ResourceConfig{cpu: cpu, memory: memory}
+}
