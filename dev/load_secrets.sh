930a8e1c03813dfed98795a4d693be2f7205d1be
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2022-11-08 12:55:50 +0700

    feat: improve dev setup (#651)
    * fix: use proper dummy app key length
    
    * feat: mount plugin to host path
    
    * feat: parameterize dag path, plugin path, and port
    
    * feat: plugin artifact via variable
    
    * feat: remove artifact path
    
    * feat: add load secret script
    
    * feat: use relative path instead
    
    * feat: adding dummy plugin
    
    * feat: multiple secret load
    
    * feat: load secret from file contains key value
    
    * feat: use load.sh to load plugin and secrets
    
    * fix: readme
    
    * feat: add image busybox on dummy plugin
    
    * refactor: gitignore and add parameterize optimus namespace
    
    * feat: add script for load artifacts
    
    * doc: update docs for mounting plugings
    
    * docs: add port-forward + fix typo
    
    * feat: add load secret message

---
+++ dev/load_secrets.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+OPTIMUS_NAMESPACE=optimus-dev
+
+PROJECT=${PROJECT:-project-a}
+HOST=${HOST:-"localhost:9100"}
+SETUP_PATH=$1
+
+is_yq_installed(){
+  if [ -z $(command -v yq) ]; then
+    >&2 echo "yq must be installed: \`brew install yq\`"
+    exit 1
+  fi
+}
+
+if [ -z "$SETUP_PATH" ]; then
+  >&2 echo "must provide setup_path: ./load_secrets.sh <setup_path>"
+  exit 1
+fi
+
+# load secrets
+echo ">> load secrets into project ${PROJECT}"
+is_yq_installed
+if ! curl --output /dev/null --silent ${HOST}/ping; then
+  >&2 echo "can't connect to optimus host ${HOST}"
+  exit 1
+fi
+
+yq '.secrets[] | (.name | . + " ") + (.value | @base64)' ${SETUP_PATH} >> s.tmp
+while read key value; do
+    resp=$(curl -XPOST -H "Content-Type: application/json" \
+    "${HOST}/api/v1beta1/project/${PROJECT}/secret/${key}" \
+    -d '{"value": "'${value}'"}' -s -o s.result -w "%{http_code}")
+    if [ $resp -eq 200 ]; then echo "set secret ${key}: success"; else echo "set secret ${key}: fail\n$(cat s.result)"; fi
+    rm s.result
+done < s.tmp && rm s.tmp
