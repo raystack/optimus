0b0b2bcaa544dbc0c05b4dd6e88edf5fe351cd98
Sandeep Bhardwaj <sandeep.bhardwaj@gojek.com>
2022-11-09 12:55:40 +0530

    refactor: add dag compile for airflow

---
+++ ext/scheduler/airflow/dag/compiler.go
@@ -0,0 +1,40 @@
+package dag
+
+import (
+	"context"
+	_ "embed"
+	"text/template"
+
+	"github.com/odpf/optimus/core/job_run"
+	"github.com/odpf/optimus/core/tenant"
+	"github.com/odpf/optimus/internal/errors"
+)
+
+//go:embed dag.py.tmpl
+var dagTemplate []byte
+
+type Compiler struct {
+	hostname string
+
+	template *template.Template
+}
+
+func (c Compiler) Compile(ctx context.Context, tnnt tenant.Tenant, job *job_run.Job) ([]byte, error) {
+	return nil, nil
+}
+
+func NewDagCompiler(hostname string) (*Compiler, error) {
+	if len(dagTemplate) == 0 {
+		return nil, errors.InternalError("SchedulerAirflow", "dag template is empty", nil)
+	}
+
+	tmpl, err := template.New("optimus_dag_compiler").Parse(string(dagTemplate))
+	if err != nil {
+		return nil, errors.InternalError("SchedulerAirflow", "unable to parse scheduler dag template", err)
+	}
+
+	return &Compiler{
+		hostname: hostname,
+		template: tmpl,
+	}, nil
+}
---
+++ ext/scheduler/airflow/dag/compiler_test.go
@@ -0,0 +1,6 @@
+package dag_test
+
+import _ "embed"
+
+//go:embed expected_compiled_template.py
+var compiledTemplate []byte
