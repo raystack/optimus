3764bd58439aa3376a88684ae8e4427b1b86ad62
Anwar Hidayat <anwarhidayat14@gmail.com>
2022-11-21 15:24:22 +0700

    Revert "feat: add store existence status in resource"
    This reverts commit cd9520df8e0cf7feed2b0817cf8be1fa15929138.

--- core/resource/resource.go
+++ core/resource/resource.go
@@ -52,8 +52,7 @@ type Resource struct {
 	spec     map[string]any
 	metadata *Metadata
 
-	status       Status
-	existInStore bool
+	status Status
 }
 
 func NewResource(fullName string, kind Kind, store Store, tnnt tenant.Tenant, meta *Metadata, spec map[string]any) (*Resource, error) {
@@ -90,14 +89,13 @@ func NewResource(fullName string, kind Kind, store Store, tnnt tenant.Tenant, me
 	}
 
 	return &Resource{
-		name:         name,
-		kind:         kind,
-		dataset:      dataset,
-		tenant:       tnnt,
-		spec:         spec,
-		metadata:     meta,
-		status:       StatusUnknown,
-		existInStore: false,
+		name:     name,
+		kind:     kind,
+		dataset:  dataset,
+		tenant:   tnnt,
+		spec:     spec,
+		metadata: meta,
+		status:   StatusUnknown,
 	}, nil
 }
 
@@ -141,10 +139,6 @@ func (r *Resource) Status() Status {
 
 func (r *Resource) Spec() map[string]any {
 	return r.spec
-}
-
-func (r *Resource) ExistInStore() bool {
-	return r.existInStore
 }
 
 func (r *Resource) Validate() error {
@@ -255,10 +249,6 @@ func (r *Resource) MarkSuccess() error {
 	}
 	msg := fmt.Sprintf("status transition for [%s] from [%s] to success is not allowed", r.FullName(), r.status)
 	return errors.InvalidStateTransition(EntityResource, msg)
-}
-
-func (r *Resource) MarkExistInStore() {
-	r.existInStore = true
 }
 
 type FromExistingOpt func(r *Resource)
