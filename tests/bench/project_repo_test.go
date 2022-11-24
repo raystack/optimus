55c1c92b14a5674e89bdf1afaec02c23c827a214
Anwar Hidayat <anwarhidayat14@gmail.com>
2022-11-24 13:55:51 +0700

    test: fix benchmark test for project repository

--- tests/bench/project_repo_test.go
+++ tests/bench/project_repo_test.go
@@ -3,89 +3,98 @@
 
 package bench
 
-//
-//func BenchmarkProjectRepository(b *testing.B) {
-//	ctx := context.Background()
-//	dbSetup := func() *gorm.DB {
-//		dbConn := setup.TestDB()
-//		setup.TruncateTables(dbConn)
-//
-//		return dbConn
-//	}
-//
-//	hash, _ := models.NewApplicationSecret("32charshtesthashtesthashtesthash")
-//
-//	b.Run("Save", func(b *testing.B) {
-//		db := dbSetup()
-//		var repo store.ProjectRepository = postgres.NewProjectRepository(db, hash)
-//		b.ResetTimer()
-//
-//		for i := 0; i < b.N; i++ {
-//			project := setup.Project(i)
-//			err := repo.Save(ctx, project)
-//			if err != nil {
-//				panic(err)
-//			}
-//		}
-//	})
-//
-//	b.Run("GetByName", func(b *testing.B) {
-//		db := dbSetup()
-//		var repo store.ProjectRepository = postgres.NewProjectRepository(db, hash)
-//		secretRepo := postgres.NewSecretRepository(db, hash)
-//		for i := 0; i < 10; i++ {
-//			project := setup.Project(i)
-//			project.ID = models.ProjectID(uuid.New())
-//			err := repo.Save(ctx, project)
-//			assert.Nil(b, err)
-//
-//			for s := 0; s < 5; s++ {
-//				secret := setup.Secret(s)
-//				err = secretRepo.Save(ctx, project, models.NamespaceSpec{}, secret)
-//				assert.Nil(b, err)
-//			}
-//		}
-//		b.ResetTimer()
-//
-//		for i := 0; i < b.N; i++ {
-//			num := i % 10
-//			projName := fmt.Sprintf("t-optimus-%d", num)
-//			prj, err := repo.GetByName(ctx, projName)
-//			if err != nil {
-//				panic(err)
-//			}
-//			if prj.Name != projName {
-//				panic("Project name is not same")
-//			}
-//		}
-//	})
-//
-//	b.Run("GetAll", func(b *testing.B) {
-//		db := dbSetup()
-//		var repo store.ProjectRepository = postgres.NewProjectRepository(db, hash)
-//		secretRepo := postgres.NewSecretRepository(db, hash)
-//		for i := 0; i < 10; i++ {
-//			project := setup.Project(i)
-//			project.ID = models.ProjectID(uuid.New())
-//			err := repo.Save(ctx, project)
-//			assert.Nil(b, err)
-//
-//			for s := 0; s < 5; s++ {
-//				secret := setup.Secret(s)
-//				err = secretRepo.Save(ctx, project, models.NamespaceSpec{}, secret)
-//				assert.Nil(b, err)
-//			}
-//		}
-//		b.ResetTimer()
-//
-//		for i := 0; i < b.N; i++ {
-//			prj, err := repo.GetAll(ctx)
-//			if err != nil {
-//				panic(err)
-//			}
-//			if len(prj) != 10 {
-//				panic("Project list is not same")
-//			}
-//		}
-//	})
-//}
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+
+	serviceTenant "github.com/odpf/optimus/core/tenant"
+	repoTenant "github.com/odpf/optimus/internal/store/postgres/tenant"
+	"github.com/odpf/optimus/tests/setup"
+)
+
+func BenchmarkProjectRepository(b *testing.B) {
+	ctx := context.Background()
+	dbSetup := func() *gorm.DB {
+		dbConn := setup.TestDB()
+		setup.TruncateTables(dbConn)
+
+		return dbConn
+	}
+
+	transporterKafkaBrokerKey := "KAFKA_BROKERS"
+	config := map[string]string{
+		"bucket":                            "gs://some_folder-2",
+		transporterKafkaBrokerKey:           "10.12.12.12:6668,10.12.12.13:6668",
+		serviceTenant.ProjectSchedulerHost:  "host",
+		serviceTenant.ProjectStoragePathKey: "gs://location",
+	}
+
+	b.Run("Save", func(b *testing.B) {
+		db := dbSetup()
+		repo := repoTenant.NewProjectRepository(db)
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			name := fmt.Sprintf("t-optimus-%d", i)
+			project, err := serviceTenant.NewProject(name, config)
+			assert.NoError(b, err)
+
+			actualError := repo.Save(ctx, project)
+			assert.NoError(b, actualError)
+		}
+	})
+
+	b.Run("GetByName", func(b *testing.B) {
+		db := dbSetup()
+		repo := repoTenant.NewProjectRepository(db)
+		maxNumberOfProjects := 50
+		for i := 0; i < maxNumberOfProjects; i++ {
+			name := fmt.Sprintf("t-optimus-%d", i)
+			project, err := serviceTenant.NewProject(name, config)
+			assert.NoError(b, err)
+
+			actualError := repo.Save(ctx, project)
+			assert.NoError(b, actualError)
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			projectIdx := i % maxNumberOfProjects
+			name := fmt.Sprintf("t-optimus-%d", projectIdx)
+			projectName, err := serviceTenant.ProjectNameFrom(name)
+			assert.NoError(b, err)
+
+			actualProject, actualError := repo.GetByName(ctx, projectName)
+			assert.NotNil(b, actualProject)
+			assert.NoError(b, actualError)
+		}
+	})
+
+	b.Run("GetAll", func(b *testing.B) {
+		db := dbSetup()
+		repo := repoTenant.NewProjectRepository(db)
+		maxNumberOfProjects := 50
+		for i := 0; i < maxNumberOfProjects; i++ {
+			name := fmt.Sprintf("t-optimus-%d", i)
+			project, err := serviceTenant.NewProject(name, config)
+			assert.NoError(b, err)
+
+			actualError := repo.Save(ctx, project)
+			assert.NoError(b, actualError)
+		}
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			actualProjects, actualError := repo.GetAll(ctx)
+			assert.Len(b, actualProjects, maxNumberOfProjects)
+			assert.NoError(b, actualError)
+		}
+	})
+}
