15143505ba3b11a5c354f38b1a4a7d5727f3072a
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2022-11-28 19:07:23 +0700

    feat: impement to job proto

--- core/job/handler/v1beta1/job_adapter.go
+++ core/job/handler/v1beta1/job_adapter.go
@@ -6,11 +6,34 @@ import (
 	"github.com/odpf/optimus/internal/utils"
 	"github.com/odpf/optimus/models"
 	pb "github.com/odpf/optimus/protos/odpf/optimus/core/v1beta1"
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
-func toJobProto(jobSpec *job.Job) *pb.JobSpecification {
-	// TODO: implement this
-	return &pb.JobSpecification{}
+func toJobProto(jobEntity *job.Job) *pb.JobSpecification {
+	return &pb.JobSpecification{
+		Version:          int32(jobEntity.Spec().Version().Int()),
+		Name:             jobEntity.Spec().Name().String(),
+		Owner:            jobEntity.Spec().Owner().String(),
+		StartDate:        jobEntity.Spec().Schedule().StartDate().String(),
+		EndDate:          jobEntity.Spec().Schedule().EndDate().String(),
+		Interval:         jobEntity.Spec().Schedule().Interval(),
+		DependsOnPast:    jobEntity.Spec().Schedule().DependsOnPast(),
+		CatchUp:          jobEntity.Spec().Schedule().CatchUp(),
+		TaskName:         jobEntity.Spec().Task().Info().Name,
+		Config:           fromConfig(jobEntity.Spec().Task().Config()),
+		WindowSize:       jobEntity.Spec().Window().GetSize(),
+		WindowOffset:     jobEntity.Spec().Window().GetOffset(),
+		WindowTruncateTo: jobEntity.Spec().Window().GetTruncateTo(),
+		Dependencies:     fromSpecUpstreams(jobEntity.Spec().Upstream()),
+		Assets:           jobEntity.Spec().Asset().Assets(),
+		Hooks:            fromHooks(jobEntity.Spec().Hooks()),
+		Description:      jobEntity.Spec().Description(),
+		Labels:           jobEntity.Spec().Labels(),
+		Behavior:         fromRetryAndAlerts(jobEntity.Spec().Schedule().Retry(), jobEntity.Spec().Alerts()),
+		Metadata:         fromMetadata(jobEntity.Spec().Metadata()),
+		Destination:      jobEntity.Destination().String(),
+		Sources:          fromResourceURNs(jobEntity.Sources()),
+	}
 }
 
 func fromJobProto(js *pb.JobSpecification) (*job.Spec, error) {
@@ -98,11 +121,42 @@ func fromJobProto(js *pb.JobSpecification) (*job.Spec, error) {
 		Build(), nil
 }
 
+func fromResourceURNs(resourceURNs []job.ResourceURN) []string {
+	var resources []string
+	for _, resourceURN := range resourceURNs {
+		resources = append(resources, resourceURN.String())
+	}
+	return resources
+}
+
+func fromRetryAndAlerts(jobRetry *job.Retry, alerts []*job.Alert) *pb.JobSpecification_Behavior {
+	retryProto := fromRetry(jobRetry)
+	notifierProto := fromAlerts(alerts)
+	if retryProto == nil && len(notifierProto) == 0 {
+		return nil
+	}
+	return &pb.JobSpecification_Behavior{
+		Retry:  retryProto,
+		Notify: notifierProto,
+	}
+}
+
 func toRetry(protoRetry *pb.JobSpecification_Behavior_Retry) *job.Retry {
 	if protoRetry == nil {
 		return nil
 	}
 	return job.NewRetry(int(protoRetry.Count), protoRetry.Delay.GetNanos(), protoRetry.ExponentialBackoff)
+}
+
+func fromRetry(jobRetry *job.Retry) *pb.JobSpecification_Behavior_Retry {
+	if jobRetry == nil {
+		return nil
+	}
+	return &pb.JobSpecification_Behavior_Retry{
+		Count:              int32(jobRetry.Count()),
+		Delay:              &durationpb.Duration{Nanos: jobRetry.Delay()},
+		ExponentialBackoff: jobRetry.ExponentialBackoff(),
+	}
 }
 
 func toHooks(hooksProto []*pb.JobSpecHook) ([]*job.Hook, error) {
@@ -121,6 +175,17 @@ func toHooks(hooksProto []*pb.JobSpecHook) ([]*job.Hook, error) {
 	return hooks, nil
 }
 
+func fromHooks(hooks []*job.Hook) []*pb.JobSpecHook {
+	var hooksProto []*pb.JobSpecHook
+	for _, hook := range hooks {
+		hooksProto = append(hooksProto, &pb.JobSpecHook{
+			Name:   hook.Name().String(),
+			Config: fromConfig(hook.Config()),
+		})
+	}
+	return hooksProto
+}
+
 func toAlerts(notifiers []*pb.JobSpecification_Behavior_Notifiers) ([]*job.Alert, error) {
 	alerts := make([]*job.Alert, len(notifiers))
 	for i, notify := range notifiers {
@@ -132,6 +197,18 @@ func toAlerts(notifiers []*pb.JobSpecification_Behavior_Notifiers) ([]*job.Alert
 		alerts[i] = job.NewAlertBuilder(alertOn, notify.Channels).WithConfig(config).Build()
 	}
 	return alerts, nil
+}
+
+func fromAlerts(jobAlerts []*job.Alert) []*pb.JobSpecification_Behavior_Notifiers {
+	var notifiers []*pb.JobSpecification_Behavior_Notifiers
+	for _, alert := range jobAlerts {
+		notifiers = append(notifiers, &pb.JobSpecification_Behavior_Notifiers{
+			On:       pb.JobEvent_Type(pb.JobEvent_Type_value[utils.ToEnumProto(string(alert.On()), "type")]),
+			Channels: alert.Channels(),
+			Config:   alert.Config().Configs(),
+		})
+	}
+	return notifiers
 }
 
 func toSpecUpstreams(upstreamProtos []*pb.JobDependency) (*job.SpecUpstream, error) {
@@ -158,6 +235,23 @@ func toSpecUpstreams(upstreamProtos []*pb.JobDependency) (*job.SpecUpstream, err
 	return upstream, nil
 }
 
+func fromSpecUpstreams(upstreams *job.SpecUpstream) []*pb.JobDependency {
+	var dependencies []*pb.JobDependency
+	for _, upstreamName := range upstreams.UpstreamNames() {
+		dependencies = append(dependencies, &pb.JobDependency{Name: upstreamName.String()}) // TODO: upstream type?
+	}
+	for _, httpUpstream := range upstreams.HTTPUpstreams() {
+		dependencies = append(dependencies, &pb.JobDependency{
+			HttpDependency: &pb.HttpDependency{
+				Url:     httpUpstream.URL(),
+				Headers: httpUpstream.Headers(),
+				Params:  httpUpstream.Params(),
+			},
+		})
+	}
+	return dependencies
+}
+
 func toMetadata(jobMetadata *pb.JobMetadata) *job.Metadata {
 	if jobMetadata == nil {
 		return nil
@@ -180,12 +274,48 @@ func toMetadata(jobMetadata *pb.JobMetadata) *job.Metadata {
 	return job.NewMetadataBuilder().WithResource(resourceMetadata).WithScheduler(schedulerMetadata).Build()
 }
 
+func fromMetadata(metadata *job.Metadata) *pb.JobMetadata {
+	if metadata == nil {
+		return nil
+	}
+
+	var metadataResourceProto *pb.JobSpecMetadataResource
+	if metadata.Resource() != nil {
+		metadataResourceProto.Request = &pb.JobSpecMetadataResourceConfig{
+			Cpu:    metadata.Resource().Request().CPU(),
+			Memory: metadata.Resource().Request().Memory(),
+		}
+		metadataResourceProto.Limit = &pb.JobSpecMetadataResourceConfig{
+			Cpu:    metadata.Resource().Limit().CPU(),
+			Memory: metadata.Resource().Limit().Memory(),
+		}
+	}
+
+	var metadataSchedulerProto *pb.JobSpecMetadataAirflow
+	if metadata.Scheduler() != nil {
+		metadataSchedulerProto.Pool = metadata.Scheduler()["pool"]
+		metadataSchedulerProto.Queue = metadata.Scheduler()["queue"]
+	}
+	return &pb.JobMetadata{
+		Resource: metadataResourceProto,
+		Airflow:  metadataSchedulerProto,
+	}
+}
+
 func toConfig(configs []*pb.JobConfigItem) (*job.Config, error) {
 	configMap := make(map[string]string, len(configs))
 	for _, config := range configs {
 		configMap[config.Name] = config.Value
 	}
 	return job.NewConfig(configMap)
+}
+
+func fromConfig(jobConfig *job.Config) []*pb.JobConfigItem {
+	configs := []*pb.JobConfigItem{}
+	for configName, configValue := range jobConfig.Configs() {
+		configs = append(configs, &pb.JobConfigItem{Name: configName, Value: configValue})
+	}
+	return configs
 }
 
 func toBasicInfoSectionProto(jobDetail *job.Job, logMessages []*pb.Log) *pb.JobInspectResponse_BasicInfoSection {
