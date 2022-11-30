ae92a29dcdabd1bd9cb9785c7c657a87a03b6d22
Arinda Arif <arindaarif05@gmail.com>
2022-12-01 06:37:05 +0700

    fix: rename indexes of job_old to avoid duplication

--- internal/store/postgres/migrations/000033_rename_job.up.sql
+++ internal/store/postgres/migrations/000033_rename_job.up.sql
@@ -1,2 +1,7 @@
 ALTER TABLE job
 RENAME TO job_old;
+
+ALTER INDEX IF EXISTS job_name_idx RENAME TO job_old_name_idx;
+ALTER INDEX IF EXISTS job_destination_idx RENAME TO job_old_destination_idx;
+ALTER INDEX IF EXISTS job_namespace_id_idx RENAME TO job_old_namespace_id_idx;
+ALTER INDEX IF EXISTS job_project_id_idx RENAME TO job_old_project_id_idx;
