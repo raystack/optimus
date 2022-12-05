c06561e07ed22b3e9194712f7d3e974ab1153a25
Arinda Arif <arindaarif05@gmail.com>
2022-12-05 15:38:55 +0700

    fix: job_run foreign key wrong add constraint statement

--- internal/store/postgres/migrations/000034_create_job.up.sql
+++ internal/store/postgres/migrations/000034_create_job.up.sql
@@ -48,4 +48,8 @@ CREATE INDEX IF NOT EXISTS job_project_name_idx ON job (project_name);
 CREATE INDEX IF NOT EXISTS job_namespace_name_idx ON job (namespace_name);
 CREATE INDEX IF NOT EXISTS job_destination_idx ON job (destination);
 
-ALTER TABLE job_run ADD CONSTRAINT IF EXISTS job_run_job_id_fkey;
+ALTER TABLE job_run
+ADD CONSTRAINT job_run_job_id_fkey
+   FOREIGN KEY (job_id)
+   REFERENCES job(id)
+   ON DELETE CASCADE;
