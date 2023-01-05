db76d44344c501be2ccd104c8c2da49e591b0bf9
Saikumar Chintada <chintadasaikumar@gmail.com>
2023-01-05 12:16:26 +0530

    fix : add JOB_NAME in executor env

--- ext/scheduler/airflow2/resources/base_dag.py
+++ ext/scheduler/airflow2/resources/base_dag.py
@@ -130,6 +130,7 @@ asset_volume_mounts = [
 executor_env_vars = [
     k8s.V1EnvVar(name="JOB_LABELS",value='{{.Job.GetLabelsAsString}}'),
     k8s.V1EnvVar(name="JOB_DIR",value=JOB_DIR),
+    k8s.V1EnvVar(name="JOB_NAME",value='{{$.Job.Name}}'),
 ]
 
 init_env_vars = [
