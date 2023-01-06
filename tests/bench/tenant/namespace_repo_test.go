ca3981a7bd0befe573954ee3d387a57322f086af
Anwar Hidayat <anwarhidayat14@gmail.com>
2023-01-06 13:27:07 +0700

    refactor: update setup function in namespace to use dedicated benchmark object

--- tests/bench/tenant/namespace_repo_test.go
+++ tests/bench/tenant/namespace_repo_test.go
@@ -17,99 +17,94 @@ import (
 )
 
 func BenchmarkNamespaceRepository(b *testing.B) {
+	const maxNumberOfNamespaces = 64
+
 	transporterKafkaBrokerKey := "KAFKA_BROKERS"
-	proj, err := serviceTenant.NewProject("t-optimus-1",
-		map[string]string{
-			"bucket":                            "gs://some_folder-2",
-			transporterKafkaBrokerKey:           "10.12.12.12:6668,10.12.12.13:6668",
-			serviceTenant.ProjectSchedulerHost:  "host",
-			serviceTenant.ProjectStoragePathKey: "gs://location",
-		})
+	config := map[string]string{
+		"bucket":                            "gs://folder_for_test",
+		transporterKafkaBrokerKey:           "192.168.1.1:8080,192.168.1.1:8081",
+		serviceTenant.ProjectSchedulerHost:  "http://localhost:8082",
+		serviceTenant.ProjectStoragePathKey: "gs://location",
+	}
+	project, err := serviceTenant.NewProject("project_for_test", config)
 	assert.NoError(b, err)
 
 	ctx := context.Background()
-	dbSetup := func() *pgxpool.Pool {
+
+	dbSetup := func(b *testing.B) *pgxpool.Pool {
+		b.Helper()
+
 		pool := setup.TestPool()
 		setup.TruncateTablesWith(pool)
 
 		prjRepo := repoTenant.NewProjectRepository(pool)
-		err := prjRepo.Save(ctx, proj)
-		if err != nil {
-			panic(err)
-		}
+		err := prjRepo.Save(ctx, project)
+		assert.NoError(b, err)
 
 		return pool
 	}
 
 	b.Run("Save", func(b *testing.B) {
-		db := dbSetup()
+		db := dbSetup(b)
 		repo := repoTenant.NewNamespaceRepository(db)
 
 		b.ResetTimer()
 
 		for i := 0; i < b.N; i++ {
-			name := fmt.Sprintf("n-optimus-%d", i)
-			ns, err := serviceTenant.NewNamespace(name, proj.Name(),
-				map[string]string{
-					"bucket": "gs://ns_bucket",
-				})
+			name := fmt.Sprintf("namespace_for_test_%d", i)
+			namespace, err := serviceTenant.NewNamespace(name, project.Name(), config)
 			assert.NoError(b, err)
 
-			actualError := repo.Save(ctx, ns)
+			actualError := repo.Save(ctx, namespace)
 			assert.NoError(b, actualError)
 		}
 	})
 
 	b.Run("GetByName", func(b *testing.B) {
-		db := dbSetup()
+		db := dbSetup(b)
 		repo := repoTenant.NewNamespaceRepository(db)
-		maxNumberOfNamespaces := 50
+		namespaceNames := make([]string, maxNumberOfNamespaces)
 		for i := 0; i < maxNumberOfNamespaces; i++ {
-			name := fmt.Sprintf("t-optimus-%d", i)
-			ns, err := serviceTenant.NewNamespace(name, proj.Name(),
-				map[string]string{
-					"bucket": "gs://ns_bucket",
-				})
+			name := fmt.Sprintf("namespace_for_test_%d", i)
+			namespace, err := serviceTenant.NewNamespace(name, project.Name(), config)
 			assert.NoError(b, err)
 
-			actualError := repo.Save(ctx, ns)
+			actualError := repo.Save(ctx, namespace)
 			assert.NoError(b, actualError)
+
+			namespaceNames[i] = name
 		}
 
 		b.ResetTimer()
 
 		for i := 0; i < b.N; i++ {
 			namespaceIdx := i % maxNumberOfNamespaces
-			name := fmt.Sprintf("t-optimus-%d", namespaceIdx)
+			name := namespaceNames[namespaceIdx]
 			namespaceName, err := serviceTenant.NamespaceNameFrom(name)
 			assert.NoError(b, err)
 
-			actualNamespace, actualError := repo.GetByName(ctx, proj.Name(), namespaceName)
+			actualNamespace, actualError := repo.GetByName(ctx, project.Name(), namespaceName)
 			assert.NotNil(b, actualNamespace)
 			assert.NoError(b, actualError)
 		}
 	})
 
 	b.Run("GetAll", func(b *testing.B) {
-		db := dbSetup()
+		db := dbSetup(b)
 		repo := repoTenant.NewNamespaceRepository(db)
-		maxNumberOfNamespaces := 50
 		for i := 0; i < maxNumberOfNamespaces; i++ {
-			name := fmt.Sprintf("t-optimus-%d", i)
-			ns, err := serviceTenant.NewNamespace(name, proj.Name(),
-				map[string]string{
-					"bucket": "gs://ns_bucket",
-				})
+			name := fmt.Sprintf("namespace_for_test_%d", i)
+			namespace, err := serviceTenant.NewNamespace(name, project.Name(), config)
 			assert.NoError(b, err)
 
-			actualError := repo.Save(ctx, ns)
+			actualError := repo.Save(ctx, namespace)
 			assert.NoError(b, actualError)
 		}
 
 		b.ResetTimer()
 
 		for i := 0; i < b.N; i++ {
-			actualNamespaces, actualError := repo.GetAll(ctx, proj.Name())
+			actualNamespaces, actualError := repo.GetAll(ctx, project.Name())
 			assert.NotNil(b, actualNamespaces)
 			assert.NoError(b, actualError)
 		}
