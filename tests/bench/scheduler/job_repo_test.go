4b33c463b1a84e595a2c65d891158263abaf2299
Anwar Hidayat <anwarhidayat14@gmail.com>
2023-01-09 10:38:08 +0700

    refactor: clean up benchmark for scheduler job repo to use builder

--- tests/bench/scheduler/job_repo_test.go
+++ tests/bench/scheduler/job_repo_test.go
@@ -21,52 +21,54 @@ import (
 )
 
 func BenchmarkJobRepository(b *testing.B) {
+	const maxNumberOfJobs = 64
+
+	transporterKafkaBrokerKey := "KAFKA_BROKERS"
+	config := map[string]string{
+		"bucket":                            "gs://folder_for_test",
+		transporterKafkaBrokerKey:           "192.168.1.1:8080,192.168.1.1:8081",
+		serviceTenant.ProjectSchedulerHost:  "http://localhost:8082",
+		serviceTenant.ProjectStoragePathKey: "gs://location",
+	}
+	project, err := serviceTenant.NewProject("project_for_test", config)
+	assert.NoError(b, err)
+	namespace, err := serviceTenant.NewNamespace("namespace_for_test", project.Name(), config)
+	assert.NoError(b, err)
+	tnnt, err := serviceTenant.NewTenant(project.Name().String(), namespace.Name().String())
+	assert.NoError(b, err)
+
 	ctx := context.Background()
 
-	proj, err := serviceTenant.NewProject("test-proj",
-		map[string]string{
-			"bucket":                            "gs://some_folder-2",
-			serviceTenant.ProjectSchedulerHost:  "host",
-			serviceTenant.ProjectStoragePathKey: "gs://location",
-		})
-	assert.NoError(b, err)
-	namespace, err := serviceTenant.NewNamespace("test-ns", proj.Name(),
-		map[string]string{
-			"bucket": "gs://ns_bucket",
-		})
-	assert.NoError(b, err)
-	tnnt, err := serviceTenant.NewTenant(proj.Name().String(), namespace.Name().String())
-	assert.NoError(b, err)
+	dbSetup := func(b *testing.B) *pgxpool.Pool {
+		b.Helper()
 
-	dbSetup := func() *pgxpool.Pool {
 		pool := setup.TestPool()
 		setup.TruncateTablesWith(pool)
 
-		projRepo := repoTenant.NewProjectRepository(pool)
-		if err := projRepo.Save(ctx, proj); err != nil {
-			panic(err)
-		}
+		projectRepo := repoTenant.NewProjectRepository(pool)
+		err := projectRepo.Save(ctx, project)
+		assert.NoError(b, err)
 
 		namespaceRepo := repoTenant.NewNamespaceRepository(pool)
-		if err := namespaceRepo.Save(ctx, namespace); err != nil {
-			panic(err)
-		}
+		err = namespaceRepo.Save(ctx, namespace)
+		assert.NoError(b, err)
+
 		return pool
 	}
 
 	b.Run("GetJob", func(b *testing.B) {
-		db := dbSetup()
+		db := dbSetup(b)
 		jobRepo := repoJob.NewJobRepository(db)
 		schedulerJobRepo := repoScheduler.NewJobProviderRepository(db)
-
-		maxNumberOfJobs := 50
 		jobs := make([]*serviceJob.Job, maxNumberOfJobs)
 		for i := 0; i < maxNumberOfJobs; i++ {
 			name := fmt.Sprintf("job_test_%d", i)
 			jobName, err := serviceJob.NameFrom(name)
 			assert.NoError(b, err)
-			destination := serviceJob.ResourceURN("dev.resource.sample")
-			jobs[i] = setup.Job(tnnt, jobName, destination)
+
+			jobs[i] = setup.NewDummyJobBuilder().
+				OverrideName(jobName).
+				Build(tnnt)
 		}
 		storedJobs, err := jobRepo.Add(ctx, jobs)
 		assert.Len(b, storedJobs, maxNumberOfJobs)
@@ -76,29 +78,30 @@ func BenchmarkJobRepository(b *testing.B) {
 
 		for i := 0; i < b.N; i++ {
 			jobIdx := i % maxNumberOfJobs
-			name := fmt.Sprintf("job_test_%d", jobIdx)
-			jobName, err := serviceScheduler.JobNameFrom(name)
+			job := jobs[jobIdx]
+			jobName, err := serviceScheduler.JobNameFrom(job.GetName())
 			assert.NoError(b, err)
 
-			actualJob, actualError := schedulerJobRepo.GetJob(ctx, proj.Name(), jobName)
+			actualJob, actualError := schedulerJobRepo.GetJob(ctx, project.Name(), jobName)
 			assert.NotNil(b, actualJob)
 			assert.NoError(b, actualError)
 		}
 	})
 
 	b.Run("GetJobDetails", func(b *testing.B) {
-		db := dbSetup()
+		db := dbSetup(b)
 		jobRepo := repoJob.NewJobRepository(db)
 		schedulerJobRepo := repoScheduler.NewJobProviderRepository(db)
 
-		maxNumberOfJobs := 50
 		jobs := make([]*serviceJob.Job, maxNumberOfJobs)
 		for i := 0; i < maxNumberOfJobs; i++ {
 			name := fmt.Sprintf("job_test_%d", i)
 			jobName, err := serviceJob.NameFrom(name)
 			assert.NoError(b, err)
-			destination := serviceJob.ResourceURN("dev.resource.sample")
-			jobs[i] = setup.Job(tnnt, jobName, destination)
+
+			jobs[i] = setup.NewDummyJobBuilder().
+				OverrideName(jobName).
+				Build(tnnt)
 		}
 		storedJobs, err := jobRepo.Add(ctx, jobs)
 		assert.Len(b, storedJobs, maxNumberOfJobs)
@@ -108,29 +111,30 @@ func BenchmarkJobRepository(b *testing.B) {
 
 		for i := 0; i < b.N; i++ {
 			jobIdx := i % maxNumberOfJobs
-			name := fmt.Sprintf("job_test_%d", jobIdx)
-			jobName, err := serviceScheduler.JobNameFrom(name)
+			job := jobs[jobIdx]
+			jobName, err := serviceScheduler.JobNameFrom(job.GetName())
 			assert.NoError(b, err)
 
-			actualJob, actualError := schedulerJobRepo.GetJobDetails(ctx, proj.Name(), jobName)
+			actualJob, actualError := schedulerJobRepo.GetJobDetails(ctx, project.Name(), jobName)
 			assert.NotNil(b, actualJob)
 			assert.NoError(b, actualError)
 		}
 	})
 
 	b.Run("GetAll", func(b *testing.B) {
-		db := dbSetup()
+		db := dbSetup(b)
 		jobRepo := repoJob.NewJobRepository(db)
 		schedulerJobRepo := repoScheduler.NewJobProviderRepository(db)
 
-		maxNumberOfJobs := 50
 		jobs := make([]*serviceJob.Job, maxNumberOfJobs)
 		for i := 0; i < maxNumberOfJobs; i++ {
 			name := fmt.Sprintf("job_test_%d", i)
 			jobName, err := serviceJob.NameFrom(name)
 			assert.NoError(b, err)
-			destination := serviceJob.ResourceURN("dev.resource.sample")
-			jobs[i] = setup.Job(tnnt, jobName, destination)
+
+			jobs[i] = setup.NewDummyJobBuilder().
+				OverrideName(jobName).
+				Build(tnnt)
 		}
 		storedJobs, err := jobRepo.Add(ctx, jobs)
 		assert.Len(b, storedJobs, maxNumberOfJobs)
@@ -139,7 +143,7 @@ func BenchmarkJobRepository(b *testing.B) {
 		b.ResetTimer()
 
 		for i := 0; i < b.N; i++ {
-			actualJobs, actualError := schedulerJobRepo.GetAll(ctx, proj.Name())
+			actualJobs, actualError := schedulerJobRepo.GetAll(ctx, project.Name())
 			assert.Len(b, actualJobs, maxNumberOfJobs)
 			assert.NoError(b, actualError)
 		}
