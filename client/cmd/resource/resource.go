cf4cf8b3e0634776c5b2f5dea20707f6ae7ce476
Anwar Hidayat <anwarhidayat14@gmail.com>
2023-01-10 15:52:08 +0700

    refactor: clean up create command for export command

--- client/cmd/resource/resource.go
+++ client/cmd/resource/resource.go
@@ -2,41 +2,18 @@ package resource
 
 import (
 	"github.com/spf13/cobra"
-
-	"github.com/odpf/optimus/config"
 )
-
-type resourceCommand struct {
-	configFilePath string
-	clientConfig   *config.ClientConfig
-}
 
 // NewResourceCommand initializes command for resource
 func NewResourceCommand() *cobra.Command {
-	resource := &resourceCommand{
-		clientConfig: &config.ClientConfig{},
-	}
-
 	cmd := &cobra.Command{
 		Use:   "resource",
 		Short: "Interact with data resource",
 		Annotations: map[string]string{
 			"group:core": "true",
 		},
-		PersistentPreRunE: resource.PersistentPreRunE,
 	}
-	cmd.PersistentFlags().StringVarP(&resource.configFilePath, "config", "c", resource.configFilePath, "File path for client configuration")
 
-	cmd.AddCommand(NewCreateCommand(resource.clientConfig))
+	cmd.AddCommand(NewCreateCommand())
 	return cmd
-}
-
-func (r *resourceCommand) PersistentPreRunE(_ *cobra.Command, _ []string) error {
-	// TODO: find a way to load the config in one place
-	c, err := config.LoadClientConfig(r.configFilePath)
-	if err != nil {
-		return err
-	}
-	*r.clientConfig = *c
-	return nil
 }
