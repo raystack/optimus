b085b36b1917f80161d52354981e295f0e7d894a
Arinda Arif <arindaarif05@gmail.com>
2023-02-21 16:12:51 +0700

    feat: add replay service

---
+++ core/scheduler/service/replay_service.go
@@ -0,0 +1,57 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"golang.org/x/net/context"
+
+	"github.com/odpf/optimus/core/scheduler"
+	"github.com/odpf/optimus/core/tenant"
+	"github.com/odpf/optimus/internal/lib/cron"
+)
+
+type ReplayRepository interface {
+	RegisterReplay(ctx context.Context, replay *scheduler.Replay) (uuid.UUID, error)
+}
+
+type ReplayValidator interface {
+	Validate(ctx context.Context, tenant tenant.Tenant, jobName scheduler.JobName, config *scheduler.ReplayConfig) error
+}
+
+type ReplayService struct {
+	replayRepo ReplayRepository
+	jobRepo    JobRepository
+
+	validator ReplayValidator
+}
+
+func (r ReplayService) CreateReplay(ctx context.Context, tenant tenant.Tenant, jobName scheduler.JobName, config *scheduler.ReplayConfig) (replayID uuid.UUID, err error) {
+	// 1. Validate
+	if err := r.validator.Validate(ctx, tenant, jobName, config); err != nil {
+		return uuid.Nil, err
+	}
+
+	// 2. Calculate replay runs
+	//    TODO: the goal for now is only to fetch the job interval. consider creating a new query in repository.
+	subjectJob, err := r.jobRepo.GetJobDetails(ctx, tenant.ProjectName(), jobName)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	jobCron, err := cron.ParseCronSchedule(subjectJob.Schedule.Interval)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("unable to parse job cron interval: %w", err)
+	}
+	runs := getExpectedRuns(jobCron, config.StartTime, config.EndTime)
+
+	// 3. Initialize replay
+	replay := scheduler.NewReplay(jobName, tenant, config, runs, scheduler.ReplayStateCreated)
+
+	// 4. Store
+	return r.replayRepo.RegisterReplay(ctx, replay)
+}
+
+func NewReplayService(replayRepo ReplayRepository, jobRepo JobRepository, validator ReplayValidator) *ReplayService {
+	return &ReplayService{replayRepo: replayRepo, jobRepo: jobRepo, validator: validator}
+}
---
+++ core/scheduler/service/replay_service_test.go
@@ -0,0 +1,189 @@
+package service_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/odpf/optimus/core/scheduler"
+	"github.com/odpf/optimus/core/scheduler/service"
+	"github.com/odpf/optimus/core/tenant"
+)
+
+func TestReplayService(t *testing.T) {
+	ctx := context.Background()
+	projName := tenant.ProjectName("proj")
+	namespaceName := tenant.ProjectName("ns1")
+	jobName := scheduler.JobName("sample_select")
+	startTimeStr := "2023-01-02T15:00:00Z"
+	startTime, _ := time.Parse(scheduler.ISODateFormat, startTimeStr)
+	endTime := startTime.Add(48 * time.Hour)
+	tnnt, _ := tenant.NewTenant(projName.String(), namespaceName.String())
+	parallel := true
+	description := "sample backfill"
+	replayConfig := scheduler.NewReplayConfig(startTime, endTime, parallel, description)
+	replayID := uuid.New()
+	job := scheduler.Job{
+		Name:   jobName,
+		Tenant: tnnt,
+	}
+	jobWithDetails := &scheduler.JobWithDetails{
+		Job: &job,
+		JobMetadata: &scheduler.JobMetadata{
+			Version: 1,
+		},
+		Schedule: &scheduler.Schedule{
+			StartDate: startTime.Add(-time.Hour * 24),
+			Interval:  "0 12 * * *",
+		},
+	}
+
+	t.Run("CreateReplay", func(t *testing.T) {
+		t.Run("should return replay ID if replay created successfully", func(t *testing.T) {
+			replayRepository := new(ReplayRepository)
+			defer replayRepository.AssertExpectations(t)
+
+			jobRepository := new(JobRepository)
+			defer jobRepository.AssertExpectations(t)
+
+			replayValidator := new(ReplayValidator)
+			defer replayValidator.AssertExpectations(t)
+
+			scheduledTime1Str := "2023-01-03T12:00:00Z"
+			scheduledTime1, _ := time.Parse(scheduler.ISODateFormat, scheduledTime1Str)
+			scheduledTime2 := scheduledTime1.Add(24 * time.Hour)
+			replayRuns := []*scheduler.JobRunStatus{
+				{ScheduledAt: scheduledTime1, State: scheduler.StatePending},
+				{ScheduledAt: scheduledTime2, State: scheduler.StatePending},
+			}
+			replayReq := scheduler.NewReplay(jobName, tnnt, replayConfig, replayRuns, scheduler.ReplayStateCreated)
+
+			replayValidator.On("Validate", ctx, tnnt, jobName, replayConfig).Return(nil)
+			jobRepository.On("GetJobDetails", ctx, projName, jobName).Return(jobWithDetails, nil)
+			replayRepository.On("RegisterReplay", ctx, replayReq).Return(replayID, nil)
+
+			replayService := service.NewReplayService(replayRepository, jobRepository, replayValidator)
+			result, err := replayService.CreateReplay(ctx, tnnt, jobName, replayConfig)
+			assert.NoError(t, err)
+			assert.Equal(t, replayID, result)
+		})
+
+		t.Run("should return error if not pass validation", func(t *testing.T) {
+			replayRepository := new(ReplayRepository)
+			defer replayRepository.AssertExpectations(t)
+
+			jobRepository := new(JobRepository)
+			defer jobRepository.AssertExpectations(t)
+
+			replayValidator := new(ReplayValidator)
+			defer replayValidator.AssertExpectations(t)
+
+			replayValidator.On("Validate", ctx, tnnt, jobName, replayConfig).Return(errors.New("not passed validation"))
+
+			replayService := service.NewReplayService(replayRepository, jobRepository, replayValidator)
+			result, err := replayService.CreateReplay(ctx, tnnt, jobName, replayConfig)
+			assert.ErrorContains(t, err, "not passed validation")
+			assert.Equal(t, uuid.Nil, result)
+		})
+
+		t.Run("should return replay ID if replay created successfully", func(t *testing.T) {
+			replayRepository := new(ReplayRepository)
+			defer replayRepository.AssertExpectations(t)
+
+			jobRepository := new(JobRepository)
+			defer jobRepository.AssertExpectations(t)
+
+			replayValidator := new(ReplayValidator)
+			defer replayValidator.AssertExpectations(t)
+
+			replayValidator.On("Validate", ctx, tnnt, jobName, replayConfig).Return(nil)
+			jobRepository.On("GetJobDetails", ctx, projName, jobName).Return(nil, errors.New("internal error"))
+
+			replayService := service.NewReplayService(replayRepository, jobRepository, replayValidator)
+			result, err := replayService.CreateReplay(ctx, tnnt, jobName, replayConfig)
+			assert.ErrorContains(t, err, "internal error")
+			assert.Equal(t, uuid.Nil, result)
+		})
+
+		t.Run("should return replay ID if replay created successfully", func(t *testing.T) {
+			replayRepository := new(ReplayRepository)
+			defer replayRepository.AssertExpectations(t)
+
+			jobRepository := new(JobRepository)
+			defer jobRepository.AssertExpectations(t)
+
+			replayValidator := new(ReplayValidator)
+			defer replayValidator.AssertExpectations(t)
+
+			scheduledTime1Str := "2023-01-03T12:00:00Z"
+			scheduledTime1, _ := time.Parse(scheduler.ISODateFormat, scheduledTime1Str)
+			scheduledTime2 := scheduledTime1.Add(24 * time.Hour)
+			replayRuns := []*scheduler.JobRunStatus{
+				{ScheduledAt: scheduledTime1, State: scheduler.StatePending},
+				{ScheduledAt: scheduledTime2, State: scheduler.StatePending},
+			}
+			replayReq := scheduler.NewReplay(jobName, tnnt, replayConfig, replayRuns, scheduler.ReplayStateCreated)
+
+			replayValidator.On("Validate", ctx, tnnt, jobName, replayConfig).Return(nil)
+			jobRepository.On("GetJobDetails", ctx, projName, jobName).Return(jobWithDetails, nil)
+			replayRepository.On("RegisterReplay", ctx, replayReq).Return(uuid.Nil, errors.New("internal error"))
+
+			replayService := service.NewReplayService(replayRepository, jobRepository, replayValidator)
+			result, err := replayService.CreateReplay(ctx, tnnt, jobName, replayConfig)
+			assert.ErrorContains(t, err, "internal error")
+			assert.Equal(t, uuid.Nil, result)
+		})
+	})
+}
+
+// ReplayRepository is an autogenerated mock type for the ReplayRepository type
+type ReplayRepository struct {
+	mock.Mock
+}
+
+// RegisterReplay provides a mock function with given fields: ctx, replay
+func (_m *ReplayRepository) RegisterReplay(ctx context.Context, replay *scheduler.Replay) (uuid.UUID, error) {
+	ret := _m.Called(ctx, replay)
+
+	var r0 uuid.UUID
+	if rf, ok := ret.Get(0).(func(context.Context, *scheduler.Replay) uuid.UUID); ok {
+		r0 = rf(ctx, replay)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(uuid.UUID)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *scheduler.Replay) error); ok {
+		r1 = rf(ctx, replay)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ReplayValidator is an autogenerated mock type for the ReplayValidator type
+type ReplayValidator struct {
+	mock.Mock
+}
+
+// Validate provides a mock function with given fields: ctx, _a1, jobName, config
+func (_m *ReplayValidator) Validate(ctx context.Context, _a1 tenant.Tenant, jobName scheduler.JobName, config *scheduler.ReplayConfig) error {
+	ret := _m.Called(ctx, _a1, jobName, config)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, tenant.Tenant, scheduler.JobName, *scheduler.ReplayConfig) error); ok {
+		r0 = rf(ctx, _a1, jobName, config)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
