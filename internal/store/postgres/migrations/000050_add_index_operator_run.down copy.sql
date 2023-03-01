1823ec11082665b6ef194a808103cc335b5353ea
Yash Bhardwaj <imyashbhardwaj@gmail.com>
2023-03-01 13:10:41 +0530

    fix: add job_run_id index on operator run tables for faster queries

--- internal/store/postgres/migrations/000050_add_index_operator_run.down copy.sql
+++ internal/store/postgres/migrations/000050_add_index_operator_run.down copy.sql
@@ -1,3 +1,3 @@
-drop index sensor_run_job_run_id;
-drop index task_run_job_run_id;
-drop index hook_run_job_run_id;
+DROP INDEX IF EXISTS sensor_run_job_run_id;
+DROP INDEX IF EXISTS task_run_job_run_id;
+DROP INDEX IF EXISTS hook_run_job_run_id;
--- internal/store/postgres/migrations/000050_add_index_operator_run.up.sql
+++ internal/store/postgres/migrations/000050_add_index_operator_run.up.sql
@@ -1,3 +1,3 @@
-CREATE INDEX sensor_run_job_run_id ON sensor_run(job_run_id);
-CREATE INDEX task_run_job_run_id ON task_run(job_run_id);
-CREATE INDEX hook_run_job_run_id ON hook_run(job_run_id);
+CREATE INDEX IF NOT EXISTS sensor_run_job_run_id ON sensor_run(job_run_id);
+CREATE INDEX IF NOT EXISTS task_run_job_run_id ON task_run(job_run_id);
+CREATE INDEX IF NOT EXISTS hook_run_job_run_id ON hook_run(job_run_id);
