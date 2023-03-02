7c2b42c3ea00f022b398e23f4440dbc4e9e6d3ef
Arinda Arif <arindaarif05@gmail.com>
2023-03-02 16:58:53 +0700

    refactor: pass job cron value to replay validate

--- core/scheduler/service/replay_service.go
+++ core/scheduler/service/replay_service.go
@@ -13,12 +13,14 @@ import (
 
 type ReplayRepository interface {
 	RegisterReplay(ctx context.Context, replay *scheduler.Replay) (uuid.UUID, error)
-	GetReplayToExecute(context.Context) (*scheduler.StoredReplay, error)
 	UpdateReplay(ctx context.Context, replayID uuid.UUID, state scheduler.ReplayState, runs []*scheduler.JobRunStatus, message string) error
+
+	GetReplayToExecute(context.Context) (*scheduler.StoredReplay, error)
+	GetReplayByStatus(ctx context.Context, statusList []scheduler.ReplayState) ([]*scheduler.StoredReplay, error)
 }
 
 type ReplayValidator interface {
-	Validate(ctx context.Context, tenant tenant.Tenant, jobName scheduler.JobName, config *scheduler.ReplayConfig) error
+	Validate(ctx context.Context, tenant tenant.Tenant, jobName scheduler.JobName, config *scheduler.ReplayConfig, jobCron *cron.ScheduleSpec) error
 }
 
 type ReplayService struct {
@@ -29,29 +31,24 @@ type ReplayService struct {
 }
 
 func (r ReplayService) CreateReplay(ctx context.Context, tenant tenant.Tenant, jobName scheduler.JobName, config *scheduler.ReplayConfig) (replayID uuid.UUID, err error) {
-	// 1. Validate
-	if err := r.validator.Validate(ctx, tenant, jobName, config); err != nil {
-		return uuid.Nil, err
-	}
-
-	// 2. Calculate replay runs
-	//    TODO: the goal for now is only to fetch the job interval. consider creating a new query in repository.
 	subjectJob, err := r.jobRepo.GetJobDetails(ctx, tenant.ProjectName(), jobName)
 	if err != nil {
 		return uuid.Nil, err
 	}
 
-	// TODO: are we expecting users to fill start time & end time of execution/scheduled?
 	jobCron, err := cron.ParseCronSchedule(subjectJob.Schedule.Interval)
 	if err != nil {
 		return uuid.Nil, fmt.Errorf("unable to parse job cron interval: %w", err)
 	}
+
+	if err := r.validator.Validate(ctx, tenant, jobName, config, jobCron); err != nil {
+		return uuid.Nil, err
+	}
+
 	runs := getExpectedRuns(jobCron, config.StartTime, config.EndTime)
 
-	// 3. Initialize replay
 	replay := scheduler.NewReplay(jobName, tenant, config, runs, scheduler.ReplayStateCreated)
 
-	// 4. Store
 	return r.replayRepo.RegisterReplay(ctx, replay)
 }
 
